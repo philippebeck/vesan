4fcaede1cf6f4b3d9b6792fd94fb226ab615e5f6
Philippe Beck <philippe@philippebeck.net>
2023-01-25 10:28:38 +0400

    Create router index

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView";
+import ContactView from "../views/ContactView";
+import LoginView from "../views/LoginView";
+import AdminView from "../views/AdminView";
+import ErrorView from "../views/ErrorView";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+    alias: ["/home"]
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    component: ContactView
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    component: AdminView,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "error",
+    component: ErrorView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
