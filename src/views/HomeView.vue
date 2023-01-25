11cf1875b6acab050c8cf886084ecd6d032f0243
Philippe Beck <philippe@philippebeck.net>
2023-01-25 10:53:43 +0400

    Create HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <CardElt id="top">
+    <template #header>
+
+      <SliderElt :slides="['html5', 'css3', 'js', 'vuejs', 'node-js']">
+        <template #slide="slotProps">
+          <i :class="`fa-brands fa-${slotProps.slide.toLowerCase()} fa-10x blue`"></i>
+        </template>
+        <template #gallery="slotProps">
+          <i :class="`fa-brands fa-${slotProps.slide.toLowerCase()}`"></i>
+        </template>
+      </SliderElt>
+
+      <h1 class="blue shatex-blur-sm anima-slideB">
+        Vesan
+      </h1>
+      <strong class="gray">
+        CMS with Vue-Elt, Servidio, Animadio & NemJS
+      </strong>
+    </template>
+
+    <template #body>
+      Welcome & I wish you to create wonderful websites !
+    </template>
+  </CardElt>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+}
+</script>
