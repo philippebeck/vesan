6026d71c9214889d0e5480cbf53fcab565a8430c
Philippe Beck <philippe@philippebeck.net>
2023-01-25 10:54:10 +0400

    Remove useless components import to App

--- src/App.vue
+++ src/App.vue
@@ -177,15 +177,8 @@
 </template>
 
 <script>
-import NavElt from "@/components/main/NavElt"
-import FootElt from "@/components/main/FootElt"
-
 export default {
   name: 'App',
-  components: {
-    NavElt,
-    FootElt
-  },
 
   data() {
     return {
