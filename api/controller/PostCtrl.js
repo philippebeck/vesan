7bbf3964c4ca569d4813db9a3de81e43256b918b
Philippe Beck <philippe@philippebeck.net>
2023-01-27 22:41:57 +0400

    Create PostCtrl

---
+++ api/controller/PostCtrl.js
@@ -0,0 +1,132 @@
+"use strict";
+
+const formidable  = require("formidable");
+const fs          = require("fs");
+const nem         = require("nemjs");
+const accents     = require("remove-accents");
+
+const PostModel = require("../model/PostModel");
+
+require("dotenv").config();
+
+const form = formidable({ 
+  uploadDir: process.env.IMG_URL, 
+  keepExtensions: true 
+});
+
+/**
+ * GET POST
+ * @param {string} title 
+ * @param {string} text 
+ * @param {string} image 
+ * @returns 
+ */
+exports.getPost = (title, text, image) => {
+
+  return {
+    title: title,
+    text: text,
+    image: image
+  }
+}
+
+/**
+ * LIST POSTS
+ * @param {object} req 
+ * @param {object} res 
+ */
+exports.listPosts = (req, res) => {
+  PostModel
+    .find()
+    .then((posts) => res.status(200).json(posts))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+//! ****************************** CRUD ******************************
+
+/**
+ * CREATE POST
+ * @param {object} req 
+ * @param {object} res 
+ * @param {function} next 
+ */
+exports.createPost = (req, res, next) => {
+  form.parse(req, (err, fields, files) => {
+
+    if (err) {
+      next(err);
+      return;
+    }
+
+    let image = this.getImgName(fields.title);
+    nem.createImage(files.image.newFilename, image);
+
+    let post = new PostModel(this.getPost(fields.title, fields.text, image));
+
+    fs.unlink(process.env.IMG_URL + files.image.newFilename, () => {
+      post
+        .save()
+        .then(() => res.status(201).json({ message: process.env.POST_CREATED }))
+        .catch((error) => res.status(400).json({ error }));
+    });
+  })
+};
+
+/**
+ * UPDATE POST
+ * @param {object} req 
+ * @param {object} res 
+ * @param {function} next 
+ */
+exports.updatePost = (req, res, next) => {
+  form.parse(req, (err, fields) => {
+
+    if (err) {
+      next(err);
+      return;
+    }
+
+    let image = fields.image;
+
+    if (Object.keys(files).length !== 0) {
+      image = this.getImgName(fields.title);
+      nem.createImage(files.image.newFilename, image);
+
+      PostModel
+        .findOne({ _id: req.params.id })
+        .then((post) => 
+          fs.unlink(process.env.IMG_URL + post.image, () => {
+            fs.unlink(process.env.IMG_URL + files.image.newFilename, () => {
+              console.log("Image ok !");
+            })
+          })
+        )
+    }
+
+    let post = this.getPost(fields.title, fields.text, image);
+
+    PostModel
+      .updateOne({ _id: req.params.id }, { ...post, _id: req.params.id })
+      .then(() => res.status(200).json({ message: process.env.POST_UPDATED }))
+      .catch((error) => res.status(400).json({ error }));
+  })
+};
+
+/**
+ * DELETE POST
+ * @param {object} req 
+ * @param {object} res 
+ */
+exports.deletePost = (req, res) => {
+  PostModel
+    .findOne({ _id: req.params.id })
+    .then(post => {
+      fs.unlink(process.env.IMG_URL + post.image, () => {
+        PostModel
+          .deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: process.env.POST_DELETED }))
+          .catch((error) => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
