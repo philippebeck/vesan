186873ab525927fe4395af1c8d3114c78367eb3c
Philippe Beck <philippe@philippebeck.net>
2023-01-28 17:32:55 +0400

    Create ListPosts component

---
+++ src/components/ListPosts.vue
@@ -0,0 +1,193 @@
+<template>
+
+  <!-- POST ADMIN MANAGER -->
+  <form method="post"
+    enctype="multipart/form-data">
+    <TableElt :items="posts"
+      id="posts">
+
+      <!-- Last Table Head -->
+      <template #head>
+        up/del
+      </template>
+
+      <!-- Current Post Image -->
+      <template #cell-_id="slotProps">
+        <MediaElt :src="'/img/posts/' + posts[slotProps.index].image"
+          :alt="'Photo de ' + posts[slotProps.index].title"
+          :title="posts[slotProps.index].image">
+        </MediaElt>
+      </template>
+
+      <!-- Post Title -->
+      <template #cell-title="slotProps">
+        <FieldElt :id="'title-' + posts[slotProps.index]._id"
+          v-model:value="getPosts()[slotProps.index].title"
+          info="Update the post title"
+          @keyup.enter="validateUpdatedPost(posts[slotProps.index]._id)">
+        </FieldElt>
+      </template>
+
+      <!-- Post Text -->
+      <template #cell-text="slotProps">
+        <FieldElt :id="'text-' + posts[slotProps.index]._id"
+          type="text"
+          v-model:value="getPosts()[slotProps.index].text"
+          info="Update the post text"
+          @keyup.enter="validateUpdatedPost(posts[slotProps.index]._id)">
+        </FieldElt>
+      </template>
+
+      <!-- Post Image -->
+      <template #cell-image="slotProps">
+        <FieldElt :id="'image-' + posts[slotProps.index]._id"
+          type="file"
+          info="Update the post image">
+        </FieldElt>
+      </template>
+
+      <!-- Post Author -->
+      <template #cell-author="slotProps">
+        <FieldElt :id="'author-' + posts[slotProps.index]._id"
+          v-model:value="author"
+          info="Update the post authorword"
+          @keyup.enter="validateUpdatedPost(posts[slotProps.index]._id)">
+        </FieldElt>
+      </template>
+
+      <template #body="slotProps">
+
+      <!-- Update Button -->
+      <BtnElt type="button"
+          @click="validateUpdatedPost(posts[slotProps.index]._id)" 
+          class="btn-sky"
+          :title="'Update ' + posts[slotProps.index].title">
+          <template #btn>
+            <i class="fa-solid fa-edit"></i>
+          </template>
+        </BtnElt>
+
+        <!-- Delete Button -->
+        <BtnElt type="button"
+          @click="deletePost(posts[slotProps.index]._id)" 
+          class="btn-red"
+          :title="'Delete ' + posts[slotProps.index].title">
+          <template #btn>
+            <i class="fa-solid fa-trash-alt"></i>
+          </template>
+        </BtnElt>
+      </template>
+    </TableElt>
+  </form>
+</template>
+
+<script>
+export default {
+  title: "ListPosts",
+  props: ["posts"],
+
+  methods: {
+    /**
+     * GET ALL POSTS
+     */
+    getPosts() {
+      return this.posts;
+    },
+
+    validateUpdatedPost(id) {
+      for (let i = 0; i < this.posts.length; i++ ) {
+        if (this.posts[i]._id === id) {
+
+          if (this.$serve.checkName(this.posts[i].title)) {
+
+            this.checkUpdatedPost(i);
+          }
+        }
+      }
+    },
+
+    /**
+     * CHECK UPDATED POST IF NAME | EMAIL ARE REFERENCED
+     * @param {number} i 
+     */
+    checkUpdatedPost(i) {
+      this.$serve.getData("/api/posts")
+        .then((posts) => {
+          let isReferenced = false;
+
+          for (let j = 0; j < posts.length; j++) {
+            if (posts[j]._id === this.posts[i]._id) {
+              posts.splice(j, 1);
+            }
+
+            if (posts[j] && posts[j].title === this.posts[i].title) {
+              alert(this.posts[i].title + " is not available !");
+              isReferenced = true;
+            }
+
+            if (posts[j] && posts[j].text === this.posts[i].text) {
+              alert(this.posts[i].text+ " is already referenced !");
+              isReferenced = true;
+            }
+          }
+
+          this.updatePost(isReferenced, i);
+        })
+        .catch(err => { console.log(err) });
+    },
+
+    /**
+     * UPDATE POST IF NO INFO IS REFERENCED
+     * @param {boolean} isReferenced 
+     * @param {number} i 
+     */
+    updatePost(isReferenced, i) {
+      if (!isReferenced) {
+
+        let post  = new FormData();
+        let image = document.getElementById('image-' + this.posts[i]._id).files[0];
+
+        if (typeof image === "undefined") {
+          image = this.posts[i].image;
+        }
+
+        post.append("id", this.posts[i]._id);
+        post.append("title", this.posts[i].title);
+        post.append("text", this.posts[i].text);
+        post.append("image", image);
+        post.append("author", this.author);
+
+        this.$serve.putData(`/api/posts/${post.get("id")}`, post)
+          .then(() => {
+            alert(post.get("title") + " updated !");
+            this.$router.go();
+          })
+          .catch(err => { console.log(err) });
+      }
+    },
+
+    /**
+     * DELETE POST
+     * @param {string} id 
+     */
+    deletePost(id) {
+      let postTitle = "";
+
+      for (let i = 0; i < this.posts.length; i++ ) {
+        if (this.posts[i]._id === id) {
+          postTitle = this.posts[i].title;
+        }
+      }
+      
+      if (confirm(`Delete ${postTitle} ?`) === true) {
+        this.$serve.deleteData(`/api/posts/${id}`)
+          .then(() => {
+            alert(postTitle + " deleted !");
+            this.$router.go();
+          })
+          .catch(err => { console.log(err) });
+      }
+    }
+  }
+}
+</script>
