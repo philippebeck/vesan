64aedde9d166872ef3266ce0ebdc2516f5053765
Philippe Beck <philippe@philippebeck.net>
2023-01-29 18:56:34 +0400

    Create ArticleView

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,5 +1,40 @@
 <template>
-  <h1>Article View</h1>
+  <CardElt>
+    <template #header>
+      <h1>Article View</h1>
+      <p>Under construction !</p>
+    </template>
 
-  <p>Under construction !</p>
+    <template #body>
+      <MediaElt :src="`/img/articles/${article.image}`"
+        :alt="article.description">
+
+        <template #figcaption>
+          <h2>{{ article.name }}</h2>
+          <p>{{ article.description }}</p>
+          <b>{{ article.price }}</b>
+        </template>
+      </MediaElt>
+    </template>
+  </CardElt>
+
+  
 </template>
+
+<script>
+export default {
+  name: "ArticleView",
+
+  data() {
+    return {
+      article: {}
+    }
+  },
+
+  mounted () {
+    this.$serve.getData(`/api/articles/${this.$route.params.id}`)
+      .then(res => { this.article = res })
+      .catch(err => { console.log(err) });
+  }
+}
+</script>
