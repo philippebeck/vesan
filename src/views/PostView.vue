d36e29dc8a53fe4b79c5a9d6619e7f4b6319b13b
Philippe Beck <philippe@philippebeck.net>
2023-01-29 19:40:58 +0400

    Create PostView

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,5 +1,39 @@
 <template>
-  <h1>Post View</h1>
+  <CardElt>
+    <template #header>
+      <h1>Article View</h1>
+      <p>Under construction !</p>
+    </template>
 
-  <p>Under construction !</p>
+    <template #body>
+      <MediaElt :src="`/img/posts/${post.image}`"
+        :alt="post.title">
+
+        <template #figcaption>
+          <h2>{{ post.title }}</h2>
+          <p>{{ post.text }}</p>
+        </template>
+      </MediaElt>
+    </template>
+  </CardElt>
+
+  
 </template>
+
+<script>
+export default {
+  name: "PostView",
+
+  data() {
+    return {
+      post: {}
+    }
+  },
+
+  mounted () {
+    this.$serve.getData(`/api/posts/${this.$route.params.id}`)
+      .then(res => { this.post = res })
+      .catch(err => { console.log(err) });
+  }
+}
+</script>
