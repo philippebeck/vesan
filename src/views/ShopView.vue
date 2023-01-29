9b23b2fd4339e31a86318c078e36d689e3467e79
Philippe Beck <philippe@philippebeck.net>
2023-01-29 12:48:02 +0400

    Create ShopView without slider

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,5 +1,114 @@
 <template>
-  <h1>Shop View</h1>
+  <NavElt
+    type="sidebar"
+    :items="cats"
+    class="sidebar">
 
-  <p>Under construction !</p>
+    <template #items>
+    </template>
+
+    <template #top>
+      <i class="fa-solid fa-chevron-circle-up fa-fw"></i>
+    </template>
+  </NavElt>
+
+  <CardElt id="top">
+    <template #header>
+
+      <!--<SliderElt :slides="getArticles()">
+        <template #slide="slotProps">
+          <img :src="`img/articles/${slotProps.slide.image}`" 
+            :alt="`${slotProps.slide.name}`">
+        </template>
+
+        <template #gallery="slotProps">
+          <img :src="`img/articles/${slotProps.slide.image}`" 
+            :alt="`${slotProps.slide.name}`">
+        </template>
+      </SliderElt>-->
+
+      <h1 class="blue shatex-blur-sm anima-slideB">
+        Shop
+      </h1>
+      <strong class="gray">
+        Articles to buy !
+      </strong>
+    </template>
+
+    <template #body>
+      <ListElt :items="itemsByCat(articles)"
+        :dynamic="true">
+
+        <template #items="slotProps">
+          <h2>{{ slotProps.item[0].cat }}</h2>
+        </template>
+
+        <template #nested="slotProps">
+          <MediaElt :src="`img/articles/${slotProps.value.image}`" 
+            :alt="`${slotProps.value.name}`" 
+            :id="`${slotProps.value.name.toLowerCase()}-${slotProps.value.cat.toLowerCase()}`">
+
+            <template #figcaption>
+              <h3>{{ slotProps.value.name }}</h3>
+              <p>{{ slotProps.value.description }}</p>
+              <b>{{ slotProps.value.price }} €</b>
+            </template>
+
+          </MediaElt>
+        </template>
+      </ListElt>
+    </template>
+  </CardElt>
 </template>
+
+<script>
+export default {
+  name: "ShopView",
+  data() {
+    return {
+      articles: []
+    }
+  },
+
+  mounted () {
+    this.$serve.getData("/api/articles")
+      .then(res => { this.articles = res })
+      .catch(err => { console.log(err) });
+  },
+
+  computed: {
+
+    cats() {
+      const cats = new Set();
+      this.articles.forEach(article => cats.add(article.cat));
+      return Array.from(cats); 
+    }
+  },
+  methods: {
+
+    /**
+     * GET ALL ARTICLES
+     */
+    getArticles() {
+      console.log(this.articles);
+      return this.articles;
+    },
+
+    /**
+     * GET ITEMS BY CATEGORY
+     * @param {*} items 
+     */
+    itemsByCat(items) {
+      const itemsByCat = {};
+      items.forEach(item => {
+        if (!itemsByCat[item.cat]) {
+          itemsByCat[item.cat] = [];
+        }
+        itemsByCat[item.cat].push(item);
+        itemsByCat[item.cat].sort((a, b) => (a.name > b.name) ? 1 : -1);
+      });
+      return itemsByCat;
+    }
+  }
+}
+</script>
