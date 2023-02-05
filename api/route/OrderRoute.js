c04296d466862f4dbdf72933861978039bc51609
Philippe Beck <philippe@philippebeck.net>
2023-02-05 15:07:50 +0400

    Add new routes to user & order routes

--- api/route/OrderRoute.js
+++ api/route/OrderRoute.js
@@ -6,10 +6,11 @@ const nem     = require("nemjs");
 
 const OrderCtrl = require("../controller/OrderCtrl");
 
+/* Public */
+router.post("/", nem.checkAuth, OrderCtrl.createOrder);
+
 /* Private */
 router.get("/", nem.checkAuth, OrderCtrl.listOrders);
-router.get("/:id", nem.checkAuth, OrderCtrl.readOrder);
-router.post("/", nem.checkAuth, OrderCtrl.createOrder);
 router.put("/:id", nem.checkAuth, OrderCtrl.updateOrder);
 router.delete("/:id", nem.checkAuth, OrderCtrl.deleteOrder);
 
--- api/route/UserRoute.js
+++ api/route/UserRoute.js
@@ -7,13 +7,15 @@ const nem     = require("nemjs");
 const UserCtrl = require("../controller/UserCtrl");
 
 /* Public */
+router.get("/check", UserCtrl.checkUser);
+router.post("/", UserCtrl.createUser);
 router.post("/login", UserCtrl.loginUser);
 router.post("/forgot", UserCtrl.forgotPass);
 router.post("/send", UserCtrl.sendMessage);
 
 /* Private */
 router.get("/", nem.checkAuth, UserCtrl.listUsers);
-router.post("/", nem.checkAuth, UserCtrl.createUser);
+router.get("/:id", nem.checkAuth, UserCtrl.readUser);
 router.put("/:id", nem.checkAuth, UserCtrl.updateUser);
 router.delete("/:id", nem.checkAuth, UserCtrl.deleteUser);
 
