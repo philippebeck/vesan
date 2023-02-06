f32355256051db5fe726cb4a3dd5d371f276d5f2
Philippe Beck <philippe@philippebeck.net>
2023-02-06 11:08:30 +0400

    Use CATS_LINK

--- src/components/managers/ListLinks.vue
+++ src/components/managers/ListLinks.vue
@@ -16,6 +16,7 @@
 
       <template #cell-_id="slotProps">
         {{ slotProps.index + 1 }}
+        ({{ slotProps.item._id }})
       </template>
 
       <template #cell-name="slotProps">
@@ -42,7 +43,7 @@
           v-model:value="table[slotProps.index].cat"
           info="Update the category"
           @keyup.enter="validateUpdatedLink(table[slotProps.index]._id)"
-          :list="['HTML5', 'CSS3', 'JS', 'PHP', 'Python', 'Git', 'Dev']">
+          :list="cats">
           {{ value }}
         </FieldElt>
       </template>
@@ -71,25 +72,23 @@
 </template>
 
 <script>
+import constants from "/constants"
+
 export default {
   name: "ListLinks",
   props: ["links"],
+
+  data() {
+    return {
+      cats: []
+    }
+  },
+
+  mounted() {
+    this.cats = constants.CATS_LINK;
+  },
+
   methods: {
-    /**
-     * RETURN AN ARRAY OF ITEMS BY CATEGORY
-     * @param {object} items 
-     */
-    itemsByCat(items) {
-      const itemsByCat = {};
-      items.forEach(item => {
-        if (!itemsByCat[item.cat]) {
-          itemsByCat[item.cat] = [];
-        }
-        itemsByCat[item.cat].push(item);
-        itemsByCat[item.cat].sort((a, b) => (a.name > b.name) ? 1 : -1);
-      });
-      return itemsByCat;
-    },
     /**
      * VALIDATE UPDATED LINK IF URL IS VALID
      * @param {string} id 
