0e63a597c76b5b5cd3141564c841beba491e714b
Philippe Beck <philippe@philippebeck.net>
2023-02-06 10:08:37 +0400

    Reorder code to OrderRoute

--- api/route/OrderRoute.js
+++ api/route/OrderRoute.js
@@ -6,11 +6,9 @@ const nem     = require("nemjs");
 
 const OrderCtrl = require("../controller/OrderCtrl");
 
-/* Public */
-router.post("/", nem.checkAuth, OrderCtrl.createOrder);
-
 /* Private */
 router.get("/", nem.checkAuth, OrderCtrl.listOrders);
+router.post("/", nem.checkAuth, OrderCtrl.createOrder);
 router.put("/:id", nem.checkAuth, OrderCtrl.updateOrder);
 router.delete("/:id", nem.checkAuth, OrderCtrl.deleteOrder);
 
