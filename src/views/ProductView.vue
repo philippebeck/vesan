ab76efa0b5bf333e14e6ee63267501f983fcaa26
Philippe Beck <philippe@philippebeck.net>
2023-02-06 21:25:45 +0400

    Remove name to ProductView order

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -209,7 +209,6 @@ export default {
     createOrder() {
       this.order = {
         id: this.product._id,
-        name: this.product.name,
         option: this.option,
         quantity: this.quantity
       };
