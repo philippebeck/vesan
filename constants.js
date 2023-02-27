89c5cb25c3b8a7d3496617b58fa37ed8c7528aca
Philippe Beck <philippe@philippebeck.net>
2023-02-27 21:49:25 +0400

    Rename & reorder constants to file

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@
 /* eslint-disable */
 
 export default {
-  /* CONFIG */
+  /******************** CONFIG ********************/
 
   API_URL : "http://localhost:3000",
 
@@ -40,9 +40,62 @@ export default {
   TOKEN : JSON.parse(localStorage.getItem("userToken")),
   USER_ID : JSON.parse(localStorage.getItem("userId")),
 
-  /* LISTS */
+  /******************** NAV ********************/
 
-CATS_ARTICLE : [
+  NAV_ADMIN : [
+    "admin", 
+    "profile", 
+    "logout"
+  ],
+
+  NAV_MAIN : [
+    "home", 
+    "shop", 
+    "blog", 
+    "contact", 
+    "basket"
+  ],
+
+  NAV_USER : [
+    "profile", 
+    "logout"
+  ],
+
+  /******************** FOOT ********************/
+
+  FOOT : [
+    "GitHub", 
+    "LinkedIn", 
+    "Medium", 
+    "NPM", 
+    "Twitter", 
+    "CodePen"
+  ],
+
+  FOOT1 : [
+    "Asperger", 
+    "Astronomy", 
+    "Philippe Beck"
+  ],
+
+  FOOT2 : [
+    "Vue-Elt", 
+    "Servidio", 
+    "Animadio", 
+    "NemJS", 
+    "Vesan"
+  ],
+
+  FOOT3 : [
+    "Links", 
+    "Legal Notice", 
+    "Donation", 
+    "Sponsor"
+  ],
+
+  /******************** CATS ********************/
+
+  CATS_ARTICLE : [
     "Transportation",
     "Business", 
     "Culture",
@@ -89,64 +142,17 @@ CATS_ARTICLE : [
     "Sports",
   ],
 
-  CONTACT_LIST : [
-    "email",
-    "subject",
-    "text"
-  ],
-
-  FOOT : [
-    "GitHub", 
-    "LinkedIn", 
-    "Medium", 
-    "NPM", 
-    "Twitter", 
-    "CodePen"
-  ],
-
-  FOOT1 : [
-    "Asperger", 
-    "Astronomy", 
-    "Philippe Beck"
-  ],
-
-  FOOT2 : [
-    "Vue-Elt", 
-    "Servidio", 
-    "Animadio", 
-    "NemJS", 
-    "Vesan"
-  ],
-
-  FOOT3 : [
-    "Links", 
-    "Legal Notice", 
-    "Donation", 
-    "Sponsor"
-  ],
+  /******************** UNDER CONSTRUCTION ********************/
 
   IS_MODERATE : [
     "true",
     "false"
   ],
 
-  NAV_ADMIN : [
-    "admin", 
-    "profile", 
-    "logout"
-  ],
-
-  NAV_MAIN : [
-    "home", 
-    "shop", 
-    "blog", 
-    "contact", 
-    "basket"
-  ],
-
-  NAV_USER : [
-    "profile", 
-    "logout"
+  CONTACT_LIST : [
+    "email",
+    "subject",
+    "text"
   ],
 
   ROLES_USER : [
@@ -164,121 +170,74 @@ CATS_ARTICLE : [
     "css3"
   ],
 
-  /* MESSAGES */
+  /******************** TITLES ********************/
 
-  ADMIN_INTRO : "Manage all data !",
-  ADMIN_TITLE : "Admin",
+  ADMIN_VIEW : "Admin",
+  ARTICLE_CREATOR : "Create article",
+  ARTICLE_MANAGER : "Article manager",
+  BASKET_VIEW : "Basket",
+  BLOG_VIEW : "Blog",
+  COMMENT_CREATOR : "Create comment",
+  COMMENT_LIST : "Comment list",
+  COMMENT_MANAGER : "Comment manager",
+  CONTACT_VIEW : "Contact",
+  ERROR_VIEW : "Error",
+  FORGOT_PASS : "Forgot password",
+  HOME_VIEW : "Vesan",
+  LEGAL_VIEW : "Apache License",
+  LINK_CREATOR : "Create link",
+  LINK_MANAGER : "Link manager",
+  LINK_VIEW : "Links",
+  ORDER_MANAGER : "Order manager",
+  PRODUCT_CREATOR : "Create product",
+  PRODUCT_MANAGER : "Product manager",
+  PROFILE_VIEW : "Profile",
+  REVIEW_CREATOR : "Create review",
+  REVIEW_LIST : "Review list",
+  REVIEW_MANAGER : "Review manager",
+  SHOP_VIEW : "Shop",
+  SIGN_IN : "Sign in",
+  SIGN_UP : "Sign up",
+  USER_MANAGER : "User manager",
 
-  ALERT_ORDER : "Order created !",
+  /******************** INTRO ********************/
 
-  BASKET_ADD : "Add to Basket ",
-  BASKET_CLEAR : "Clear the basket",
-  BASKET_EMPTY : "Your basket is empty !",
-  BASKET_INTRO : "Use the Paypal button below to order",
-  BASKET_LOGIN : "Login to order those Products",
-  BASKET_ORDER : "Order those Products",
-  BASKET_TITLE : "Basket",
-  BASKET_TOTAL : "The total of your basket is",
+  INTRO_ADMIN : "Manage all data !",
+  INTRO_BASKET : "Use the Paypal button below to order",
+  INTRO_BLOG : "Articles to read !",
+  INTRO_CONTACT : "Contact me if you have any question",
+  INTRO_ERROR : "Page not found !",
+  INTRO_FORGOT : "Need a new password ?",
+  INTRO_HOME : "CMS with Vue-Elt, Servidio, Animadio & NemJS",
+  INTRO_LEGAL : "Version 2.0, January 2004",
+  INTRO_LINK : "Links for dev !",
+  INTRO_PROFILE : "Manage your profile & your orders",
+  INTRO_SHOP : "Products to buy !",
+  INTRO_SIGNIN : "Sign in to your account !",
+  INTRO_SIGNUP : "Sign up to comment & review !",
+  MANAGE_SHOP : "Manage products, reviews & orders",
+  MANAGE_BLOG : "Manage articles & comments",
+  MANAGE_USERS : "Manage users",
+  MANAGE_LINKS : "Manage links",
 
-  BLOG_TITLE : "Blog",
-  BLOG_INTRO : "Articles to read !",
+  /******************** INFO ********************/
 
-  BUTTON_FORGOT : "Send a new password",
-  BUTTON_MESSAGE : "Send a message",
-  BUTTON_SIGNIN : "SignIn to your account",
-  BUTTON_SIGNUP : "SignUp a new user",
+  INFO_ALT : "Alternative text",
+  INFO_CATEGORY : "Choose a category",
+  INFO_EMAIL : "john@doe.com",
+  INFO_IMAGE : "Image file only",
+  INFO_NAME : "My new name",
+  INFO_OPTION : "Select an option",
+  INFO_OPTIONS : "option-1,option-2,option-3,etc",
+  INFO_PASSWORD : "********",
+  INFO_PRICE : "100 €",
+  INFO_QUANTITY : "Choose a quantity",
+  INFO_SCORE : "From 0 to 5",
+  INFO_SUBJECT : "The subject of your message ?",
+  INFO_TEXT : "Once upon a time...",
+  INFO_URL : "https://",
 
-  CHECK_AVAILABLE : " is not available !",
-  CHECK_NAME : "The Name must have between 2 & 50 characters.",
-  CHECK_EMAIL : "Your Email is not a valid address.",
-  CHECK_PASS : "Your Password must have 8 to 50 characters, with uppercase, lowercase, 1 number minimum & no space.",
-  CHECK_REFERENCE : " is already referenced !",
-  CHECK_REVIEW : "You have already written a review for this product, you can edit your old review or delete it",
-  CHECK_URL : "This URL is not a valid path.",
-  CHECK_TEXT : "The text must have between 8 & 5000 characters.",
-
-  CONFIRM_BASKET : "Do you want to clear your basket ?",
-
-  CONTACT_INTRO : "Contact me if you have any question",
-  CONTACT_TITLE : "Contact",
-
-  CONTENT_CLEAR : "Clear",
-  CONTENT_CREATE : "Create",
-  CONTENT_ENTER : "Enter",
-  CONTENT_ORDER : "Order",
-  CONTENT_SEND : "Send",
-
-  CREATE_ALT : "Alternative text",
-  CREATE_ARTICLE : "Create article",
-  CREATE_BY : "Created by",
-  CREATE_CATEGORY : "Choose a category",
-  CREATE_COMMENT : "Create comment",
-  CREATE_DESCRIPTION : "This is wonderful !",
-  CREATE_EMAIL : "john@doe.com",
-  CREATE_IMAGE : "Image file only",
-  CREATE_LINK : "Create link",
-  CREATE_NAME : "My new name",
-  CREATE_OPTION : "Select an option",
-  CREATE_OPTIONS : "option-1,option-2,option-3,etc",
-  CREATE_PASSWORD : "********",
-  CREATE_PRICE : "100 €",
-  CREATE_PRODUCT : "Create product",
-  CREATE_QUANTITY : "Choose a quantity",
-  CREATE_REVIEW : "Create review",
-  CREATE_SCORE : "From 0 to 5",
-  CREATE_SUBJECT : "The subject of your message ?",
-  CREATE_TEXT : "Once upon a time...",
-  CREATE_TITLE : "My new title",
-  CREATE_URL : "https://",
-
-  DELETE : "Delete ",
-  DELETE_ACCOUNT : "Delete your account",
-  DELETE_COMMENT : "Delete comment #",
-  DELETE_ORDER : "Delete order #",
-  DELETE_REVIEW : "Delete review #",
-
-  DISLIKE : "Dislike ",
-  DISLIKED : " disliked !",
-
-  ERROR_ALT : "404 Error",
-  ERROR_TITLE : "Page not found !",
-  ERROR_LINK : "Go back home ?",
-
-  FORGOT_CONFIRM : `, do you confirm sending a new password ?`,
-  FORGOT_CREDENTIALS : "Wrong email or password !",
-  FORGOT_EMAIL : "This email is not referenced",
-  FORGOT_INTRO : "Need a new password ?",
-  FORGOT_SUBJECT : "New password",
-  FORGOT_TEXT : "This is your new password, keep it safe : ",
-  FORGOT_TITLE : "Forgot password",
-
-  GO_TO : "Go to ",
-
-  HEAD_MOD : "mod/del",
-  HEAD_UP : "up/del",
-
-  HOME_INTRO : "CMS with Vue-Elt, Servidio, Animadio & NemJS",
-  HOME_TITLE : "Vesan",
-  HOME_GO : "Go back Home !",
-
-  IMG_ARTICLE : "An image of the article must be uploaded !",
-  IMG_PRODUCT : "An image of the product must be uploaded !",
-
-  LABEL_ALT : "Write the alternative text",
-  LABEL_CATEGORY : "Choose the most appropriate category",
-  LABEL_DESCRIPTION : "Write the description",
-  LABEL_EMAIL : "Write your email",
-  LABEL_IMAGE : "Provide an image",
-  LABEL_NAME : "Write a name",
-  LABEL_OPTION : "Indicate the product option",
-  LABEL_OPTIONS : "Write the options",
-  LABEL_PASSWORD : "8 to 50 characters with upper & lower, 1 number mini & no space",
-  LABEL_PRICE : "Write the price",
-  LABEL_QUANTITY : "Indicate the product quantity",
-  LABEL_SCORE : "Write the score",
-  LABEL_SUBJECT : "Write the subject",
-  LABEL_TEXT : "Write the text",
-  LABEL_TITLE : "Write the title",
+  /******************** LEGEND ********************/
 
   LEGEND_ALT : "Alt",
   LEGEND_CATEGORY : "Category",
@@ -297,44 +256,131 @@ CATS_ARTICLE : [
   LEGEND_TITLE : "Title",
   LEGEND_URL : "Url",
 
+  /******************** LABEL ********************/
+  
+  LABEL_ALT : "Write the alternative text",
+  LABEL_CATEGORY : "Choose the most appropriate category",
+  LABEL_DESCRIPTION : "Write the description",
+  LABEL_EMAIL : "Write your email",
+  LABEL_IMAGE : "Provide an image",
+  LABEL_NAME : "Write a name",
+  LABEL_OPTION : "Indicate the product option",
+  LABEL_OPTIONS : "Write the options",
+  LABEL_PASSWORD : "8 to 50 characters with upper & lower, 1 number mini & no space",
+  LABEL_PRICE : "Write the price",
+  LABEL_QUANTITY : "Indicate the product quantity",
+  LABEL_SCORE : "Write the score",
+  LABEL_SUBJECT : "Write the subject",
+  LABEL_TEXT : "Write the text",
+  LABEL_TITLE : "Write the title",
+  LABEL_URL : "Write the URL",
+
+  /******************** UPDATE ********************/
+
+  UPDATE_ALT : "Update the alternative text",
+  UPDATE_CATEGORY : "Update the category",
+  UPDATE_COMMENT : "Update comment #",
+  UPDATE_DESCRIPTION : "Update the description",
+  UPDATE_EMAIL : "Update the email",
+  UPDATE_IMAGE : "Update the image",
+  UPDATE_MODERATE : "Update moderation",
+  UPDATE_NAME : "Update the name",
+  UPDATE_OPTIONS : "Update the options",
+  UPDATE_PRICE : "Update the price",
+  UPDATE_PROFILE : "Update your profile",
+  UPDATE_QUANTITY : "Update the quantity",
+  UPDATE_ORDER : "Update status of order #",
+  UPDATE_REVIEW : "Update review #",
+  UPDATE_ROLE : "Update the role",
+  UPDATE_SCORE : "Update the score",
+  UPDATE_STATUS : "Update the status",
+  UPDATE_TEXT : "Update the text",
+  UPDATE_URL : "Update the URL",
+
+  /******************** CONTENT ********************/
+
+  CONTENT_ADD : "Add",
+  CONTENT_CLEAR : "Clear",
+  CONTENT_CREATE : "Create",
+  CONTENT_ENTER : "Enter",
+  CONTENT_ORDER : "Order",
+  CONTENT_SEND : "Send",
+
+  /******************** TITLE ********************/
+
+  TITLE_CLEAR : "Clear the basket",
+  TITLE_FORGOT : "Send a new password",
+  TITLE_MESSAGE : "Send a message",
+  TITLE_SIGNIN : "SignIn to your account",
+  TITLE_SIGNUP : "SignUp a new user",
+
+  /******************** SERVIDIO CHECK ********************/
+
+  CHECK_NAME : "The Name must have between 2 & 50 characters.",
+  CHECK_EMAIL : "Your Email is not a valid address.",
+  CHECK_PASS : "Your Password must have 8 to 50 characters, with uppercase, lowercase, 1 number minimum & no space.",
+  CHECK_URL : "This URL is not a valid path.",
+  CHECK_TEXT : "The text must have between 8 & 5000 characters.",
+
+  /******************** UNDER CONSTRUCTION ********************/
+
+  ALERT_ORDER : "Order created !",
+
+  BASKET_CONFIRM : "Do you want to clear your basket ?",
+  BASKET_EMPTY : "Your basket is empty !",
+  BASKET_LOGIN : "Login to order those Products",
+  BASKET_ORDER : "Order those Products",
+  BASKET_TOTAL : "The total of your basket is",
+
+  CHECK_AVAILABLE : " is not available !",
+
+  CHECK_REFERENCE : " is already referenced !",
+  CHECK_REVIEW : "You have already written a review for this product, you can edit your old review or delete it",
+
+  CREATE_BY : "Created by",
+
+  DELETE : "Delete ",
+  DELETE_ACCOUNT : "Delete your account",
+  DELETE_COMMENT : "Delete comment #",
+  DELETE_ORDER : "Delete order #",
+  DELETE_REVIEW : "Delete review #",
+  DELETED : " deleted !",
+
+  DISLIKE : "Dislike ",
+  DISLIKED : " disliked !",
+
+  ERROR_ALT : "404 Error",
+  ERROR_LINK : "Go back home ?",
+
+  FORGOT_CONFIRM : `, do you confirm sending a new password ?`,
+  FORGOT_CREDENTIALS : "Wrong email or password !",
+  FORGOT_EMAIL : "This email is not referenced",
+  FORGOT_SUBJECT : "New password",
+  FORGOT_TEXT : "This is your new password, keep it safe : ",
+
+  GO_HOME : "Go back Home !",
+  GO_TO : "Go to ",
+
+  HEAD_MOD : "mod/del",
+  HEAD_UP : "up/del",
+
+  IMG_ARTICLE : "An image of the article must be uploaded !",
+  IMG_PRODUCT : "An image of the product must be uploaded !",
+
   LIKE : "Like ",
   LIKE_LOGIN : "Login to like ",
   LIKED : " liked !",
-
-  LINK_INTRO : "Links for dev !",
-  LINK_TITLE : "Links",
-
-  LIST_COMMENTS : "Comment list",
-  LIST_REVIEWS : "Review list",
-
-  MANAGE_SHOP : "Manage products, reviews & orders",
-  MANAGE_BLOG : "Manage articles & comments",
-  MANAGE_USERS : "Manage users",
-  MANAGE_LINKS : "Manage links",
-
-  MANAGER_ARTICLE : "Article manager",
-  MANAGER_COMMENT : "Comment manager",
-  MANAGER_LINK : "Link manager",
-  MANAGER_ORDER : "Order manager",
-  MANAGER_PRODUCT : "Product manager",
-  MANAGER_REVIEW : "Review manager",
-  MANAGER_USER : "User manager",
 
   MODERATE_COMMENT : "Moderate comment #",
   MODERATE_REVIEW : "Moderate review #",
 
   ON : "on",
 
-  ORDER_TITLE : "Your orders",
-
   PAYPAL_BTN : "Failed to render the PayPal Buttons",
   PAYPAL_CANCEL : "Canceled transaction !",
   PAYPAL_ERROR : "Invalid transaction !",
   PAYPAL_SDK : "Failed to load the PayPal JS SDK script",
   PAYPAL_STATUS : "Status of transaction #",
-
-  PROFILE_INTRO : "Manage your profile & your orders",
-  PROFILE_TITLE : "Profile",
 
   READ : "Read ",
 
@@ -345,42 +391,14 @@ CATS_ARTICLE : [
   REVIEW_READ : "Read reviews about ",
   REVIEW_WRITE : "Write a review",
 
-  SHOP_TITLE : "Shop",
-  SHOP_INTRO : "Products to buy !",
-
-  SIGNIN_INTRO : "Sign in to your account !",
-  SIGNIN_TITLE : "Sign in",
-
-  SIGNUP_INTRO : "Sign up to comment & review !",
-  SIGNUP_TITLE : "Sign up",
+  TITLE_ORDER : "Your orders",
+  TITLE_USER : "Users",
 
   TOTAL : "Total",
 
   UPDATE : "Update ",
-  UPDATE_ALT : "Update the alternative text",
-  UPDATE_CATEGORY : "Update the category",
-  UPDATE_COMMENT : "Update comment #",
-  UPDATE_DESCRIPTION : "Update the description",
-  UPDATE_EMAIL : "Update the email",
-  UPDATE_IMAGE : "Update the image",
-  UPDATE_MODERATE : "Update moderation",
-  UPDATE_NAME : "Update the name",
   UPDATE_ON : "Updated on",
-  UPDATE_OPTIONS : "Update the options",
-  UPDATE_PASSWORD : "Update the password",
-  UPDATE_PRICE : "Update the price",
-  UPDATE_PROFILE : "Update your profile",
-  UPDATE_QUANTITY : "Update the quantity",
-  UPDATE_ORDER : "Update status of order #",
-  UPDATE_REVIEW : "Update review #",
-  UPDATE_ROLE : "Update the role",
-  UPDATE_SCORE : "Update the score",
-  UPDATE_STATUS : "Update the status",
-  UPDATE_TEXT : "Update the text",
-  UPDATE_TITLE : "Update the title",
-  UPDATE_URL : "Update the URL",
-
-  USER_TITLE : "Users",
+  UPDATED : " updated !",
 
   WATCH : "Watch ",
 }
