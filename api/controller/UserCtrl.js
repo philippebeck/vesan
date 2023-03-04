f54374a1ede480f0a460f379eafd1a9141d55df5
Philippe Beck <philippe@philippebeck.net>
2023-03-04 16:08:29 +0400

    Remove auth methods from UserCtrl

--- api/controller/UserCtrl.js
+++ api/controller/UserCtrl.js
@@ -123,26 +123,6 @@ exports.updateImage = (id, name, newFilename) => {
 //! ****************************** PUBLIC ******************************
 
 /**
- * READ AVATAR
- * @param {object} req 
- * @param {object} res 
- */
-exports.readAvatar = (req, res) => {
-  UserModel
-    .findById(req.params.id)
-    .then((user) => { 
-      let avatar = {};
-
-      avatar.name   = user.name;
-      avatar.image  = user.image;
-      avatar.role   = user.role;
-
-      res.status(200).json(avatar) 
-    })
-    .catch((error) => res.status(404).json({ error }));
-}
-
-/**
  * CREATE USER
  * @param {object} req 
  * @param {object} res 
@@ -190,81 +170,6 @@ exports.createUser = (req, res, next) => {
       })
       .catch((error) => { res.status(404).json({ error }) });
   });
-}
-
-/**
- * FORGOT PASSWORD
- * @param {object} req 
- * @param {object} res 
- * @param {function} next 
- */
-exports.forgotPass = (req, res, next) => {
-  form.parse(req, (err, fields) => {
-
-    if (err) {
-      next(err);
-      return;
-    }
-
-    if (!nem.checkEmail(fields.email)) {
-      return res.status(403).json({ message: process.env.CHECK_EMAIL });
-    }
-
-    UserModel
-      .findOne({ email: fields.email })
-      .then((user) => {
-        if (user !== null) {
-          let pass = nem.generatePass();
-
-          fields.html = `
-            <p>${fields.html}</p>
-            <b>${pass}</b>
-          `;
-
-          console.log(user);
-
-          bcrypt
-            .hash(pass, 10)
-            .then((hash) => {
-              let newUser = this.getUser(
-                user.name, user.email, user.image, hash, 
-                user.role, user.created, user.updated
-              );
-
-              UserModel
-                .findByIdAndUpdate(user._id, { ...newUser, _id: user._id })
-                .then(() => { this.setMessage(fields, res) })
-                .catch((error) => res.status(400).json({ error }));
-            })
-            .catch((error) => res.status(400).json({ error }));
-
-        } else {
-          return res.status(403).json({ message: process.env.DISPO_EMAIL_REF });
-        }
-      })
-      .catch((error) => res.status(404).json({ error }));
-  })
-}
-
-/**
- * LOGIN USER
- * @param {object} req 
- * @param {object} res 
- * @param {function} next 
- */
-exports.loginUser = (req, res, next) => {
-  form.parse(req, (err, fields) => {
-
-    if (err) {
-      next(err);
-      return;
-    }
-
-    UserModel
-      .findOne({ email: fields.email })
-      .then((user) => { nem.checkLogin(fields.pass, user, res) })
-      .catch((error) => res.status(401).json({ error }));
-  })
 }
 
 /**
