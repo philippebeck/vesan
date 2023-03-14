c516ac83ac24e2cd884c6dcee9d922b0d0ac4f99
Philippe Beck <philippe@philippebeck.net>
2023-03-14 20:44:15 +0400

    Create meta for editors

--- src/views/private/ProductEditor.vue
+++ src/views/private/ProductEditor.vue
@@ -172,6 +172,15 @@ export default {
     }
   },
 
+  created() {
+    this.$serve.setMeta(
+      this.constants.HEAD_PRODUCT, 
+      this.constants.META_PRODUCT,
+      this.constants.UI_URL,
+      this.constants.UI_URL + "/img/logo.svg"
+    );
+  },
+
   mounted() {
     if (this.constants.USER_ID) {
       this.$serve.getData("/auth/" + this.constants.USER_ID)
