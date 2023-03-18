cee04604d167bde40ccba83d811d25ffbf17e895
Philippe Beck <philippe@philippebeck.net>
2023-03-18 20:46:08 +0400

    Remove global vue-elt & create assets for local components

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,59 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
     concat: {
-      options: {
-        banner: "/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> | <%= pkg.license %> */\n\n", 
-        footer: "\n/* Author: <%= pkg.author.name %> <<%= pkg.author.email %>>\n Updated: <%= grunt.template.today('dS mmm yyyy @ h:MM:ss TT') %> */"
+      media: {
+        src: [
+          "node_modules/vue-elt/src/lib-components/base/MediaElt.vue",
+        ], 
+        dest: "src/assets/MediaElt.vue"
+      },
+      field: {
+        src: [
+          "node_modules/vue-elt/src/lib-components/base/FieldElt.vue",
+        ], 
+        dest: "src/assets/FieldElt.vue"
+      },
+      btn: {
+        src: [
+          "node_modules/vue-elt/src/lib-components/base/BtnElt.vue",
+        ], 
+        dest: "src/assets/BtnElt.vue"
+      },
+      slider: {
+        src: [
+          "node_modules/vue-elt/src/lib-components/data/SliderElt.vue",
+        ], 
+        dest: "src/assets/SliderElt.vue"
+      },
+      list: {
+        src: [
+          "node_modules/vue-elt/src/lib-components/data/ListElt.vue",
+        ], 
+        dest: "src/assets/ListElt.vue"
+      },
+      table: {
+        src: [
+          "node_modules/vue-elt/src/lib-components/data/TableElt.vue",
+        ], 
+        dest: "src/assets/TableElt.vue"
+      },
+      nav: {
+        src: [
+          "node_modules/vue-elt/src/lib-components/main/NavElt.vue",
+        ], 
+        dest: "src/assets/NavElt.vue"
+      },
+      card: {
+        src: [
+          "node_modules/vue-elt/src/lib-components/main/CardElt.vue",
+        ], 
+        dest: "src/assets/CardElt.vue"
+      },
+      foot: {
+        src: [
+          "node_modules/vue-elt/src/lib-components/main/FootElt.vue",
+        ], 
+        dest: "src/assets/FootElt.vue"
       },
       css: {
         src: [
