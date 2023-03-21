00631357c2d10990be678896d048f2f9667e9fef
Philippe Beck <philippe@philippebeck.net>
2023-03-21 10:01:40 +0400

    Refactoring gruntfile for lines

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,66 +2,17 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
     concat: {
-      media: {
-        src: [
-          "node_modules/vue-elt/src/lib-components/base/MediaElt.vue",
-        ], 
-        dest: "src/assets/MediaElt.vue"
-      },
-      field: {
-        src: [
-          "node_modules/vue-elt/src/lib-components/base/FieldElt.vue",
-        ], 
-        dest: "src/assets/FieldElt.vue"
-      },
-      btn: {
-        src: [
-          "node_modules/vue-elt/src/lib-components/base/BtnElt.vue",
-        ], 
-        dest: "src/assets/BtnElt.vue"
-      },
-      slider: {
-        src: [
-          "node_modules/vue-elt/src/lib-components/data/SliderElt.vue",
-        ], 
-        dest: "src/assets/SliderElt.vue"
-      },
-      list: {
-        src: [
-          "node_modules/vue-elt/src/lib-components/data/ListElt.vue",
-        ], 
-        dest: "src/assets/ListElt.vue"
-      },
-      table: {
-        src: [
-          "node_modules/vue-elt/src/lib-components/data/TableElt.vue",
-        ], 
-        dest: "src/assets/TableElt.vue"
-      },
-      nav: {
-        src: [
-          "node_modules/vue-elt/src/lib-components/main/NavElt.vue",
-        ], 
-        dest: "src/assets/NavElt.vue"
-      },
-      card: {
-        src: [
-          "node_modules/vue-elt/src/lib-components/main/CardElt.vue",
-        ], 
-        dest: "src/assets/CardElt.vue"
-      },
-      foot: {
-        src: [
-          "node_modules/vue-elt/src/lib-components/main/FootElt.vue",
-        ], 
-        dest: "src/assets/FootElt.vue"
-      },
-      css: {
-        src: [
-          "public/css/import.css",
-          "node_modules/vue-elt/dist/style.css",
-          "public/css/overload.css"
-        ], 
+      media: { src: ["node_modules/vue-elt/src/lib-components/base/MediaElt.vue"], dest: "src/assets/MediaElt.vue" },
+      field: { src: ["node_modules/vue-elt/src/lib-components/base/FieldElt.vue"], dest: "src/assets/FieldElt.vue" },
+      btn: { src: ["node_modules/vue-elt/src/lib-components/base/BtnElt.vue"], dest: "src/assets/BtnElt.vue" },
+      slider: { src: ["node_modules/vue-elt/src/lib-components/data/SliderElt.vue"], dest: "src/assets/SliderElt.vue" },
+      list: { src: ["node_modules/vue-elt/src/lib-components/data/ListElt.vue"], dest: "src/assets/ListElt.vue"},
+      table: { src: ["node_modules/vue-elt/src/lib-components/data/TableElt.vue"], dest: "src/assets/TableElt.vue" },
+      nav: { src: ["node_modules/vue-elt/src/lib-components/main/NavElt.vue"], dest: "src/assets/NavElt.vue" },
+      card: { src: ["node_modules/vue-elt/src/lib-components/main/CardElt.vue"], dest: "src/assets/CardElt.vue" },
+      foot: { src: ["node_modules/vue-elt/src/lib-components/main/FootElt.vue"], dest: "src/assets/FootElt.vue" },
+      css: { 
+        src: ["public/css/import.css", "node_modules/vue-elt/dist/style.css", "public/css/overload.css"], 
         dest: "public/css/style.ui.css"
       }
     }
