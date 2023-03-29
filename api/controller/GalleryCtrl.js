402a4cb5a429da07e54c8ae5eae1825eacd80de4
Philippe Beck <philippe@philippebeck.net>
2023-03-29 17:02:15 +0400

    Continue implementation of GalleryCtrl

--- api/controller/GalleryCtrl.js
+++ api/controller/GalleryCtrl.js
@@ -8,6 +8,9 @@ const GalleryModel  = require("../model/GalleryModel");
 const ImageModel    = require("../model/ImageModel");
 
 require("dotenv").config();
+
+const GALLERIES_IMG = process.env.IMG_URL + "galleries/";
+const GALLERIES_THUMB = process.env.THUMB_URL + "galleries/";
 const form = formidable();
 
 //! ****************************** CHECKERS ******************************
@@ -16,49 +19,28 @@ const form = formidable();
  * CHECK GALLERY DATA
  * @param {string} name 
  * @param {string} author 
- * @param {number} year 
- * @param {string} url 
  * @param {object} res 
  */
-exports.checkGalleryData = (name, author, year, url, res) => {
-  let alert = "";
-
-  if (!nem.checkUrl(url)) { 
-    alert = process.env.CHECK_URL 
-  }
-
-  if (!nem.checkNumber(year)) { 
-    alert = process.env.CHECK_YEAR 
-  }
-
+exports.checkGalleryData = (name, author, res) => {
   if (!nem.checkString(author)) { 
-    alert = process.env.CHECK_NAME 
+    return res.status(403).json({ message: process.env.CHECK_NAME });
   }
 
   if (!nem.checkString(name)) { 
-    alert = process.env.CHECK_NAME 
-  }
-
-  if (alert !== "") { 
-    return res.status(403).json({ message: alert }) 
+    return res.status(403).json({ message: process.env.CHECK_NAME });
   }
 }
 
 /**
  * CHECK GALLERY UNIQUE
  * @param {string} name 
- * @param {string} url 
  * @param {object} gallery 
  * @param {object} res 
  * @returns
  */
-exports.checkGalleryUnique = (name, url, gallery, res) => {
+exports.checkGalleryUnique = (name, gallery, res) => {
   if (gallery.name === name) {
     return res.status(403).json({ message: process.env.DISPO_NAME });
-  }
-
-  if (gallery.url === url) {
-    return res.status(403).json({ message: process.env.DISPO_URL });
   }
 }
 
@@ -66,34 +48,14 @@ exports.checkGalleryUnique = (name, url, gallery, res) => {
  * CHECK GALLERIES FOR UNIQUE
  * @param {string} id 
  * @param {array} galleries 
- * @param {object} fields 
+ * @param {string} name 
  * @param {object} res 
  */
-exports.checkGalleriesForUnique = (id, galleries, fields, res) => {
+exports.checkGalleriesForUnique = (id, galleries, name, res) => {
   for (let gallery of galleries) {
     if (!gallery._id.equals(id)) { 
-      this.checkGalleryUnique(fields.name, fields.url, gallery, res) 
+      this.checkGalleryUnique(name, gallery, res) 
     }
-  }
-}
-
-//! ****************************** GETTER ******************************
-
-/**
- * GET GALLERY
- * @param {string} name 
- * @param {string} author 
- * @param {number} year 
- * @param {string} url 
- * @returns 
- */
-exports.getGallery = (name, author, year, url) => {
-
-  return {
-    name: name,
-    author: author,
-    year: year,
-    url: url
   }
 }
 
@@ -107,17 +69,7 @@ exports.getGallery = (name, author, year, url) => {
 exports.listGalleries = (req, res) => {
   GalleryModel
     .find()
-    .then((galleries) => {
-
-      UserModel
-        .find()
-        .then((users) => {
-
-          galleries = nem.getArrayWithUsername(galleries, users);
-          res.status(200).json(galleries);
-        })
-      .catch(() => res.status(404).json({ message: process.env.USERS_NOT_FOUND }));
-    })
+    .then((galleries) => { res.status(200).json(galleries) })
     .catch(() => res.status(404).json({ message: process.env.GALLERIES_NOT_FOUND }));
 }
 
@@ -129,17 +81,7 @@ exports.listGalleries = (req, res) => {
 exports.readGallery = (req, res) => {
   GalleryModel
   .findById(req.params.id)
-  .then((gallery) => {
-
-    UserModel
-      .findById(gallery.user)
-      .then((user) => {
-
-        gallery.user = user.name;
-        res.status(200).json(gallery);
-      })
-      .catch(() => res.status(404).json({ message: process.env.USER_NOT_FOUND }));
-  })
+  .then((gallery) => { res.status(200).json(gallery) })
   .catch(() => res.status(404).json({ message: process.env.GALLERY_NOT_FOUND }));
 }
 
@@ -155,26 +97,25 @@ exports.createGallery = (req, res, next) => {
   form.parse(req, (err, fields, files) => {
     if (err) { next(err); return }
 
-    this.checkGalleryData(fields.name, fields.author, fields.year, fields.url, res);
+    this.checkGalleryData(fields.name, fields.author, res);
 
     GalleryModel
       .find()
       .then((galleries) => {
         for (let gallery of galleries) { 
-          this.checkGalleryUnique(fields.name, fields.author, gallery, res) 
+          this.checkGalleryUnique(fields.name, gallery, res) 
         }
 
-        let image = nem.getImageName(fields.name);
+        let cover = nem.getGalleryCoverName(fields.name);
 
-        nem.setThumbnail("galleries/" + files.image.newFilename, "galleries/" + image);
-
-        let gallery = new GalleryModel(this.getGalleryCreated(
-          fields.name, fields.author, fields.year, fields.url
-        ));
+        let gallery = new GalleryModel({
+          name: fields.name,
+          author: fields.author,
+          cover: cover
+        });
 
         gallery
           .save()
-          .then(() => fs.unlink(GALLERIES_IMG + files.image.newFilename, () => {}))
           .then(() => res.status(201).json({ message: process.env.GALLERY_CREATED }))
           .catch(() => res.status(400).json({ message: process.env.GALLERY_NOT_CREATED }));
       })
@@ -192,22 +133,17 @@ exports.updateGallery = (req, res, next) => {
   form.parse(req, (err, fields, files) => {
     if (err) { next(err); return }
 
-    this.checkGalleryData(fields.name, fields.text, fields.alt, fields.cat, res);
+    this.checkGalleryData(fields.name, fields.author, res);
 
     GalleryModel
       .find()
       .then((galleries) => {
-        this.checkGalleriesForUnique(req.params.id, galleries, fields, res);
+        this.checkGalleriesForUnique(req.params.id, galleries, fields.name, res);
 
-        let image = fields.image;
-
-        if (Object.keys(files).length !== 0) { 
-          image = this.getImageUpdated(req.params.id, fields.name, files.image.newFilename, res) 
-        }
-
-        let gallery = this.getGalleryUpdated(
-          fields.name, fields.author, fields.year, fields.url
-        );
+        let gallery = {
+          name: fields.name,
+          author: fields.author
+        };
 
         GalleryModel
           .findByIdAndUpdate(req.params.id, { ...gallery, _id: req.params.id })
@@ -224,24 +160,24 @@ exports.updateGallery = (req, res, next) => {
  * @param {object} res 
  */
 exports.deleteGallery = (req, res) => {
-  GalleryModel
-    .findById(req.params.id)
-    .then(gallery => {
-      fs.unlink(GALLERIES_THUMB + gallery.image, () => {
-        fs.unlink(GALLERIES_IMG + gallery.image, () => {
-
-          ImageModel
-            .deleteMany({ gallery: req.params.id })
-            .then(() =>
-
-              GalleryModel
-                .findByIdAndDelete(req.params.id)
-                .then(() => res.status(204).json({ message: process.env.GALLERY_DELETED }))
-                .catch(() => res.status(400).json({ message: process.env.GALLERY_NOT_DELETED }))
-            )
-            .catch(() => res.status(400).json({ message: process.env.IMAGE_DELETE_MANY }));
+  ImageModel
+    .find({ gallery: req.params.id })
+    .then(images => {
+      for (let image of images) {
+        fs.unlink(GALLERIES_THUMB + image.name, () => {
+          fs.unlink(GALLERIES_IMG + image.name, () => {});
         });
-      })
+      }
+      ImageModel
+        .deleteMany({ gallery: req.params.id })
+        .then(() =>
+
+          GalleryModel
+            .findByIdAndDelete(req.params.id)
+            .then(() => res.status(204).json({ message: process.env.GALLERY_DELETED }))
+            .catch(() => res.status(400).json({ message: process.env.GALLERY_NOT_DELETED }))
+        )
+        .catch(() => res.status(400).json({ message: process.env.IMAGE_DELETE_MANY }));
     })
-    .catch(() => res.status(404).json({ message: process.env.GALLERY_NOT_FOUND }));
+    .catch(() => res.status(404).json({ message: process.env.IMAGES_NOT_FOUND }));
 }
