ecfc26aa6d31390e7cef0dee3acbdf090bce3762
Philippe Beck <philippe@philippebeck.net>
2023-04-14 09:51:35 +0400

    Improve tests for all connectors

--- tests/unit/components/connectors/SignIn.spec.js
+++ tests/unit/components/connectors/SignIn.spec.js
@@ -1,13 +1,32 @@
-import { mount } from "@vue/test-utils"
 import SignIn from "@/components/connectors/SignIn"
 
 describe("SignIn", () => {
-
-  test("has data", () => {
-    expect(typeof SignIn.data).toBe("function")
+  test("name", () => { 
+    expect(SignIn.name).toBe("SignIn") 
   })
 
-  test("has props", () => {
-    expect(typeof SignIn.props).toBe("object")
+  test("components", () => { 
+    expect(typeof SignIn.components).toBe("object") 
+    expect(typeof SignIn.components.BtnElt).toBe("object") 
+    expect(typeof SignIn.components.FieldElt).toBe("object") 
+    expect(typeof SignIn.components.VueRecaptcha).toBe("object") 
+  })
+
+  test("props", () => { 
+    expect(typeof SignIn.props).toBe("object") 
+    expect(SignIn.props).toContain("constants") 
+  })
+
+  test("data", () => { 
+    expect(typeof SignIn.data).toBe("function") 
+    expect(SignIn.data()).toEqual({ 
+      email: "", 
+      pass: ""
+    }) 
+  })
+
+  test("methods", () => { 
+    expect(typeof SignIn.methods.onVerify).toBe("function") 
+    expect(typeof SignIn.methods.signIn).toBe("function") 
   })
 })
--- tests/unit/components/connectors/SignUp.spec.js
+++ tests/unit/components/connectors/SignUp.spec.js
@@ -1,13 +1,35 @@
-import { mount } from "@vue/test-utils"
 import SignUp from "@/components/connectors/SignUp"
 
-describe("ForgotPass", () => {
-
-  test("has data", () => {
-    expect(typeof SignUp.data).toBe("function")
+describe("SignUp", () => {
+  test("name", () => { 
+    expect(SignUp.name).toBe("SignUp") 
   })
 
-  test("has props", () => {
-    expect(typeof SignUp.props).toBe("object")
+  test("components", () => { 
+    expect(typeof SignUp.components).toBe("object") 
+    expect(typeof SignUp.components.BtnElt).toBe("object") 
+    expect(typeof SignUp.components.FieldElt).toBe("object") 
+    expect(typeof SignUp.components.ListElt).toBe("object") 
+    expect(typeof SignUp.components.VueRecaptcha).toBe("object") 
+  })
+
+  test("props", () => { 
+    expect(typeof SignUp.props).toBe("object") 
+    expect(SignUp.props).toContain("constants") 
+  })
+
+  test("data", () => { 
+    expect(typeof SignUp.data).toBe("function") 
+    expect(SignUp.data()).toEqual({ 
+      name: "", 
+      email: "", 
+      image: "", 
+      pass: ""
+    }) 
+  })
+
+  test("methods", () => { 
+    expect(typeof SignUp.methods.onVerify).toBe("function") 
+    expect(typeof SignUp.methods.createUser).toBe("function") 
   })
 })
