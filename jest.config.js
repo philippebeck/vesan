0bb4a220d1137bed715585fab6fe15418c6f20ad
Philippe Beck <philippe@philippebeck.net>
2023-04-23 14:25:19 +0400

    Fix transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -167,8 +167,7 @@ module.exports = {
  // transform: undefined,
  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
-    "/node_modules/(?!axios)",
-    "/node_modules/(?!servidio)"
+    "/node_modules/(?!serve)"
   ],
  // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
  // unmockedModulePathPatterns: undefined,
