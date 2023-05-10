d8e60656bd70e33caa0a5be8abab092aa81d48ee
Philippe Beck <philippe@philippebeck.net>
2023-05-10 14:32:18 +0400

    New tests for LoginView wrapper

--- src/tests/unit/views/LoginView.spec.js
+++ src/tests/unit/views/LoginView.spec.js
@@ -1,34 +1,57 @@
+import { shallowMount, enableAutoUnmount } from "@vue/test-utils"
+import serve from "../../../assets/serve.js"
 import LoginView from "../../../views/LoginView"
 
+let wrapper;
+
+/**
+ * @jest-environment jsdom
+ */
+beforeEach(() => {
+  jest.spyOn(serve, "setMeta").mockImplementation(() => {});
+
+  wrapper = shallowMount(LoginView, {
+    props: {
+      constants: {
+        TEST: "test"
+      }
+    },
+    data() {
+      return {
+        type: "SignUp"
+      }
+    }
+  });
+});
+
+enableAutoUnmount(afterEach)
+
 describe("LoginView", () => {
-  test("name", () => { 
-    expect(LoginView.name).toBe("LoginView") 
+  test("wrapper must be a vue instance", () => {
+    expect(wrapper.exists()).toBe(true)
   })
 
-  test("components", () => { 
-    expect(typeof LoginView.components).toBe("object") 
-    expect(typeof LoginView.components.BtnElt).toBe("object") 
-    expect(typeof LoginView.components.CardElt).toBe("object") 
-    expect(typeof LoginView.components.SignUp).toBe("object") 
-    expect(typeof LoginView.components.SignIn).toBe("object") 
-    expect(typeof LoginView.components.ForgotPass).toBe("object") 
+  test("wrapper components", () => { 
+    expect(typeof wrapper.findComponent({ name: "BtnElt" })).toBe("object")
+    expect(typeof wrapper.findComponent({ name: "CardElt" })).toBe("object")
+    expect(typeof wrapper.findComponent({ name: "SignUp" })).toBe("object")
+    expect(typeof wrapper.findComponent({ name: "SignIn" })).toBe("object")
+    expect(typeof wrapper.findComponent({ name: "ForgotPass" })).toBe("object")
   })
 
-  test("props", () => { 
-    expect(typeof LoginView.props).toBe("object") 
-    expect(LoginView.props).toContain("constants") 
+  test("wrapper props", () => { 
+    expect(wrapper.props().constants).toStrictEqual({ TEST: "test" })
   })
 
-  test("data", () => { 
-    expect(typeof LoginView.data).toBe("function") 
-    expect(LoginView.data()).toEqual({ type: "SignIn" }) 
+  test("wrapper data", () => { 
+    expect(wrapper.vm.type).toBe("SignUp")
   })
 
-  test("created()", () => {
-    expect(typeof LoginView.created).toBe("function")
+  test("wrapper created hook", () => {
+    expect(serve.setMeta).toHaveBeenCalled()
   })
 
-  test("methods", () => { 
-    expect(typeof LoginView.methods.setType).toBe("function") 
+  test("wrapper methods", () => { 
+    expect(typeof wrapper.vm.setType).toBe("function")
   })
 })
