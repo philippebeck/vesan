2a37a9d554e1ae6e2f05ee8d88a1a62c3eb1d6e5
Philippe Beck <philippe@philippebeck.net>
2023-05-10 19:50:59 +0400

    Clean tests to GalleryCreator

--- src/tests/unit/components/GalleryCreator.spec.js
+++ src/tests/unit/components/GalleryCreator.spec.js
@@ -8,13 +8,13 @@ beforeEach(() => {
     props: {
       constants: {
         TEST: "test"
-      },
-      name: "Gallery name", 
-      author: "Gallery author" 
+      }
     },
-    global: {
-      mocks: {},
-      stubs: {}
+    data() {
+      return {
+        name: "Gallery name", 
+        author: "Gallery author"
+      }
     }
   });
 });
@@ -22,52 +22,29 @@ beforeEach(() => {
 enableAutoUnmount(afterEach)
 
 describe("GalleryCreator", () => {
-  test("name", () => { 
-    expect(GalleryCreator.name).toBe("GalleryCreator") 
-  })
-
-  test("components", () => { 
-    expect(typeof GalleryCreator.components).toBe("object") 
-  })
-
-  test("props", () => { 
-    expect(typeof GalleryCreator.props).toBe("object") 
-  })
-
-  test("data", () => { 
-    expect(typeof GalleryCreator.data).toBe("function") 
-  })
-
-  test("methods", () => { 
-    expect(typeof GalleryCreator.methods).toBe("object") 
-  })
-})
-
-describe("Mounted GalleryCreator", () => {
   test("wrapper", () => { 
     expect(wrapper.exists()).toBe(true) 
   })
 
-  test("wrapper components", () => { 
+  test("components", () => { 
     expect(typeof wrapper.findComponent({ name: "BtnElt" })).toBe("object")
     expect(typeof wrapper.findComponent({ name: "CardElt" })).toBe("object")
     expect(typeof wrapper.findComponent({ name: "FieldElt" })).toBe("object")
     expect(typeof wrapper.findComponent({ name: "ListElt" })).toBe("object")
   })
 
-  test("wrapper props", () => {
+  test("props", () => {
     expect(wrapper.props("constants")).toStrictEqual({ TEST: "test" })
   })
 
-  test("wrapper data", () => {
-    expect(wrapper.vm.name).toBe("")
-    expect(wrapper.vm.author).toBe("")
-
-    expect(wrapper.attributes("name")).toBe("Gallery name")
-    expect(wrapper.attributes("author")).toBe("Gallery author")
+  test("data", () => {
+    expect(wrapper.vm.$data).toStrictEqual({
+      name: "Gallery name",
+      author: "Gallery author"
+    })
   })
 
-  test("wrapper methods", () => { 
+  test("methods", () => { 
     expect(typeof wrapper.vm.createGallery).toBe("function") 
   })
 });
