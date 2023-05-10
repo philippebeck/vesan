c74bbe7d399e0996a3500926286630384d6b4102
Philippe Beck <philippe@philippebeck.net>
2023-05-10 23:29:53 +0400

    Improve ForgotPass tests

--- src/tests/unit/components/ForgotPass.spec.js
+++ src/tests/unit/components/ForgotPass.spec.js
@@ -1,29 +1,50 @@
+import { shallowMount, enableAutoUnmount } from "@vue/test-utils"
 import ForgotPass from "../../../components/ForgotPass"
 
+let wrapper;
+
+/**
+ * @jest-environment jsdom
+ */
+beforeEach(() => {
+
+  wrapper = shallowMount(ForgotPass, {
+    props: {
+      constants: {
+        TEST: "test"
+      }
+    },
+    data() {
+      return {
+        email: "email@test.com"
+      }
+    }
+  });
+});
+
+enableAutoUnmount(afterEach)
+
 describe("ForgotPass", () => {
-  test("name", () => { 
-    expect(ForgotPass.name).toBe("ForgotPass") 
+  test("wrapper", () => { 
+    expect(wrapper.exists()).toBe(true)
   })
 
   test("components", () => { 
-    expect(typeof ForgotPass.components).toBe("object") 
-    expect(typeof ForgotPass.components.BtnElt).toBe("object") 
-    expect(typeof ForgotPass.components.FieldElt).toBe("object") 
-    expect(typeof ForgotPass.components.VueRecaptcha).toBe("object") 
+    expect(typeof wrapper.findComponent({ name: "BtnElt" })).toBe("object")
+    expect(typeof wrapper.findComponent({ name: "FieldElt" })).toBe("object")
+    expect(typeof wrapper.findComponent({ name: "VueRecaptcha" })).toBe("object")
   })
 
   test("props", () => { 
-    expect(typeof ForgotPass.props).toBe("object") 
-    expect(ForgotPass.props).toContain("constants") 
+    expect(wrapper.props().constants.TEST).toBe("test")
   })
 
   test("data", () => { 
-    expect(typeof ForgotPass.data).toBe("function") 
-    expect(ForgotPass.data()).toEqual({ email: "" }) 
+    expect(wrapper.vm.email).toBe("email@test.com")
   })
 
   test("methods", () => { 
-    expect(typeof ForgotPass.methods.onVerify).toBe("function") 
-    expect(typeof ForgotPass.methods.forgotPass).toBe("function") 
+    expect(typeof wrapper.vm.onVerify).toBe("function")
+    expect(typeof wrapper.vm.forgotPass).toBe("function")
   })
 })
