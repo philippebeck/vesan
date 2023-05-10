01ef8a347d153724e39b7045d2eccfff206c9534
Philippe Beck <philippe@philippebeck.net>
2023-05-10 14:31:31 +0400

    New tests for LegalView wrapper

--- src/tests/unit/views/LegalView.spec.js
+++ src/tests/unit/views/LegalView.spec.js
@@ -1,22 +1,45 @@
+import { shallowMount, enableAutoUnmount } from "@vue/test-utils"
+import serve from "../../../assets/serve.js"
 import LegalView from "../../../views/LegalView"
 
+let wrapper;
+
+/**
+ * @jest-environment jsdom
+ */
+beforeEach(() => {
+  jest.spyOn(serve, "setMeta").mockImplementation(() => {});
+
+  wrapper = shallowMount(LegalView, {
+    props: {
+      constants: {
+        TEST: "test"
+      },
+      user: {
+        name: "test",
+        email: "email@test.com"
+      }
+    }
+  });
+});
+
+enableAutoUnmount(afterEach)
+
 describe("LegalView", () => {
-  test("name", () => { 
-    expect(LegalView.name).toBe("LegalView") 
+  test("wrapper must be a vue instance", () => {
+    expect(wrapper.exists()).toBe(true)
   })
 
-  test("components", () => { 
-    expect(typeof LegalView.components).toBe("object") 
-    expect(typeof LegalView.components.CardElt).toBe("object") 
+  test("wrapper components", () => { 
+    expect(typeof wrapper.findComponent({ name: "CardElt" })).toBe("object") 
   })
 
-  test("props", () => { 
-    expect(typeof LegalView.props).toBe("object") 
-    expect(LegalView.props).toContain("constants") 
-    expect(LegalView.props).toContain("user") 
+  test("wrapper props", () => { 
+    expect(wrapper.props("constants")).toStrictEqual({ TEST: "test" })
+    expect(wrapper.props("user")).toStrictEqual({ name: "test", email: "email@test.com" })
   })
 
-  test("created()", () => {
-    expect(typeof LegalView.created).toBe("function")
+  test("wrapper created hook", () => {
+    expect(serve.setMeta).toHaveBeenCalled()
   })
 })
