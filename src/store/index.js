2f2745a48a0b7fde862073cfb08256b3eabc5bde
Philippe Beck <philippe@philippebeck.net>
2023-05-13 19:52:17 +0400

    Add JSDoc & comments to store

--- src/store/index.js
+++ src/store/index.js
@@ -7,13 +7,15 @@ export default createStore({
   namespaced: true,
 
   state: {
-    // OBJECT
+    //! ******************** OBJECT STATE ********************
+
     article:  {},
     gallery:  {},
     product:  {},
     user:     {},
 
-    // ARRAY
+    //! ******************** ARRAY STATE ********************
+
     articles:   [],
     comments:   [],
     galleries:  [],
@@ -26,13 +28,16 @@ export default createStore({
   },
 
   getters: {
-    // OBJECT
+
+    //! ******************** OBJECT GETTERS ********************
+
     getArticle: state => state.article,
     getGallery: state => state.gallery,
     getProduct: state => state.product,
     getUser:    state => state.user,
 
-    // ARRAY
+    //! ******************** ARRAY GETTERS ********************
+
     getArticles:  state => state.articles,
     getComments:  state => state.comments,
     getGalleries: state => state.galleries,
@@ -45,13 +50,15 @@ export default createStore({
   },
 
   mutations: {
-    // OBJECT
+    //! ******************** OBJECT MUTATIONS ********************
+
     SET_ARTICLE(state, article) { state.article = article },
     SET_GALLERY(state, gallery) { state.gallery = gallery },
     SET_PRODUCT(state, product) { state.product = product },
     SET_USER(state, user) { state.user = user },
 
-    // ARRAY
+    //! ******************** ARRAY MUTATIONS ********************
+
     SET_ARTICLES(state, articles) { state.articles = articles },
     SET_COMMENTS(state, comments) { state.comments = comments },
     SET_GALLERIES(state, galleries) { state.galleries = galleries },
@@ -70,112 +77,197 @@ export default createStore({
   },
 
   actions: {
-    // PUBLIC OBJECT
+    //! ******************** PUBLIC OBJECT ********************
+
+    /**
+     * ! READ ARTICLE
+     * @param {object} context 
+     * @param {string} id 
+     */
     async readArticle(context, id) {
       fetchGet(constants.API_URL + "/articles/" + id)
         .then(res => { context.commit("SET_ARTICLE", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! READ AVATAR
+     * @param {object} context 
+     * @param {string} id 
+     */
     async readAvatar(context, id) {
       fetchGet(constants.API_URL + "/auth/" + id)
         .then(res => { context.commit("SET_USER", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! READ GALLERY
+     * @param {object} context 
+     * @param {string} id 
+     */
     async readGallery(context, id) {
       fetchGet(constants.API_URL + "/galleries/" + id)
         .then(res => { context.commit("SET_GALLERY", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! READ PRODUCT
+     * @param {object} context 
+     * @param {string} id 
+     */
     async readProduct(context, id) {
       fetchGet(constants.API_URL + "/products/" + id)
         .then(res => { context.commit("SET_PRODUCT", res) })
         .catch(err => { checkError(err) });
     },
 
-    // PRIVATE OBJECT
+    //! ******************** PRIVATE OBJECT ********************
+
+    /**
+     * ! READ USER
+     * @param {object} context 
+     * @param {string} id 
+     */
     async readUser(context, id) {
       getData("/users/" + id)
         .then(res => { context.commit("SET_USER", res) })
         .catch(err => { checkError(err) });
     },
 
-    // PUBLIC ARRAY
+    //! ******************** PUBLIC ARRAY ********************
+
+    /**
+     * ! LIST ARTICLE COMMENTS
+     * @param {object} context 
+     * @param {string} id 
+     */
     async listArticleComments(context, id) {
       fetchGet(constants.API_URL + "/comments/" + id)
         .then(res => { context.commit("SET_COMMENTS", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST ARTICLES
+     * @param {object} context 
+     */
     async listArticles(context) {
       fetchGet(constants.API_URL + "/articles")
         .then(res => { context.commit("SET_ARTICLES", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST COMMENTS
+     * @param {object} context 
+     */
     async listComments(context) {
       fetchGet(constants.API_URL + "/comments")
         .then(res => { context.commit("SET_COMMENTS", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST GALLERIES
+     * @param {object} context 
+     */
     async listGalleries(context) {
       fetchGet(constants.API_URL + "/galleries")
         .then(res => { context.commit("SET_GALLERIES", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST GALLERY IMAGES
+     * @param {object} context 
+     * @param {string} id 
+     */
     async listGalleryImages(context, id) {
       fetchGet(constants.API_URL + "/images/" + id)
         .then(res => { context.commit("SET_IMAGES", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST LINKS
+     * @param {object} context 
+     */
     async listLinks(context) {
       fetchGet(constants.API_URL + "/links")
         .then(res => { context.commit("SET_LINKS", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST PRODUCT REVIEWS
+     * @param {object} context 
+     * @param {string} id 
+     */
     async listProductReviews(context, id) {
       fetchGet(constants.API_URL + "/reviews/" + id)
         .then(res => { context.commit("SET_REVIEWS", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST PRODUCTS
+     * @param {object} context 
+     */
     async listProducts(context) {
       fetchGet(constants.API_URL + "/products")
         .then(res => { context.commit("SET_PRODUCTS", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST REVIEWS
+     * @param {object} context 
+     */
     async listReviews(context) {
       fetchGet(constants.API_URL + "/reviews")
         .then(res => { context.commit("SET_REVIEWS", res) })
         .catch(err => { checkError(err) });
     },
 
-    // PRIVATE ARRAY
+    //! ******************** PRIVATE ARRAY ********************
+
+    /**
+     * ! LIST IMAGES
+     * @param {object} context 
+     */
     async listImages(context) {
       getData("/images")
         .then(res => { context.commit("SET_IMAGES", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST USER ORDERS
+     * @param {object} context 
+     * @param {string} id 
+     */
     async listUserOrders(context, id) {
       getData("/orders/" + id)
         .then(res => { context.commit("SET_USER_ORDERS", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST ORDERS
+     * @param {object} context 
+     */
     async listOrders(context) {
       getData("/orders")
         .then(res => { context.commit("SET_ORDERS", res) })
         .catch(err => { checkError(err) });
     },
 
+    /**
+     * ! LIST USERS
+     * @param {object} context 
+     */
     async listUsers(context) {
       getData("/users")
         .then(res => { context.commit("SET_USERS", res) })
