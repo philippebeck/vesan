6c3adf4d29954f4941e53d4dd378f2abc336fef9
Philippe Beck <philippe@philippebeck.net>
2023-11-25 17:53:29 +0400

    Fix servidio path to store

--- src/config/store.js
+++ src/config/store.js
@@ -1,6 +1,6 @@
-import { createStore } from "vuex"
-import { getData, setError } from "../assets/serve"
 import constants from "./constants"
+import { getData, setError } from "servidio"
+import { createStore } from "vuex"
 
 export default createStore({
   namespaced: true,
