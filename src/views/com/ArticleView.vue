924f00f59057063df52897b9cbf8d9ca3ae94161
Philippe Beck <philippe@philippebeck.net>
2023-12-12 22:10:28 +0400

    Merge views & editors for article & product

--- src/views/com/ArticleView.vue
+++ src/views/com/ArticleView.vue
@@ -84,22 +84,137 @@
         </MediaElt>
       </template>
     </CardElt>
+
+    <CardElt v-if="checkSession('admin') || checkSession('editor')"
+      id="create-article">
+      <template #header>
+        <h2>{{ constants.EDIT }} {{ article.name }}</h2>
+      </template>
+
+      <template #body>
+        <form method="post"
+          enctype="multipart/form-data">
+          <ListElt :items="constants.ARTICLE_FORM">
+
+            <template #item-1>
+              <FieldElt v-model:value="article.name"
+                @keyup.enter="updateArticle()"
+                :info="constants.INFO_NAME"
+                :min="2">
+
+                <template #legend>
+                  {{ constants.LEGEND_NAME }}
+                </template>
+                <template #label>
+                  {{ constants.LABEL_NAME }}
+                </template>
+              </FieldElt>
+            </template>
+
+            <template #item-2>
+              <label for="text">
+                {{ constants.LEGEND_TEXT }}
+              </label>
+
+              <Editor id="text"
+                :api-key="constants.TINY_KEY"
+                v-model="article.text"
+                :init="{
+                  toolbar:
+                    'undo redo outdent indent align lineheight | \
+                    bold italic underline strikethrough backcolor | \
+                    blocks fontfamily fontsize'
+                }"/>
+            </template>
+
+            <template #item-3>
+              <MediaElt v-if="article.image"
+                :src="'/img/thumbnails/articles/' + article.image"
+                :alt="article.alt" />
+
+              <FieldElt id="image"
+                type="file"
+                v-model:value="image"
+                :info="constants.INFO_IMAGE">
+
+                <template #legend>
+                  {{ constants.LEGEND_IMAGE }}
+                </template>
+                <template #label>
+                  {{ constants.LABEL_IMAGE }}
+                </template>
+              </FieldElt>
+            </template>
+
+            <template #item-4>
+              <FieldElt type="textarea"
+                v-model:value="article.alt"
+                @keyup.enter="updateArticle()"
+                :info="constants.INFO_ALT">
+
+                <template #legend>
+                  {{ constants.LEGEND_ALT }}
+                </template>
+                <template #label>
+                  {{ constants.LABEL_ALT }}
+                </template>
+              </FieldElt>
+            </template>
+
+            <template #item-5>
+              <FieldElt type="select"
+                :list="constants.CATS_ARTICLE"
+                v-model:value="article.cat"
+                @keyup.enter="updateArticle()"
+                :info="constants.INFO_CATEGORY">
+
+                <template #legend>
+                  {{ constants.LEGEND_CATEGORY }}
+                </template>
+                <template #label>
+                  {{ constants.LABEL_CATEGORY }}
+                </template>
+              </FieldElt>
+            </template>
+          </ListElt>
+          <br>
+
+          <BtnElt type="button"
+            @click="updateArticle()" 
+            class="btn-sky"
+            :content="constants.CONTENT_UPDATE"
+            :title="constants.TITLE_UPDATE + article.name">
+
+            <template #btn>
+              <i class="fa-solid fa-cloud-arrow-up fa-lg"></i>
+            </template>
+          </BtnElt>
+        </form>
+      </template>
+    </CardElt>
   </main>
 </template>
 
 <script>
 import BtnElt from "@/assets/elements/BtnElt"
 import CardElt from "@/assets/elements/CardElt"
+import FieldElt from "@/assets/elements/FieldElt"
+import ListElt from "@/assets/elements/ListElt"
 import MediaElt from "@/assets/elements/MediaElt"
 
-import { checkRole, getData, putData, setError, setMeta } from "servidio"
+import Editor from "@tinymce/tinymce-vue"
+
+import { checkRange, checkRole, getData, putData, setError, setMeta } from "servidio"
 
 export default {
   name: "ArticleView",
   components: {
     BtnElt,
     CardElt,
-    MediaElt
+    FieldElt,
+    ListElt,
+    MediaElt,
+    Editor
   },
 
   props: ["constants", "user"],
@@ -121,6 +236,7 @@ export default {
           this.constants.UI_URL + "/article/" + article.id,
           this.constants.UI_URL + "/img/thumbnails/articles/" + article.image
         );
+
       }))
       .catch(err => { 
         setError(err);
@@ -190,6 +306,41 @@ export default {
           }
         })
         .catch(err => { setError(err) });
+    },
+
+    /**
+     * UPDATE ARTICLE
+     */
+    updateArticle() {
+      const MAX = this.constants.TEXT_MAX;
+      const MIN = this.constants.TEXT_MIN;
+      const MSG = this.constants.CHECK_STRING;
+
+      if (checkRange(this.article.name, MSG) && 
+          checkRange(this.article.text, MSG, MIN, MAX) && 
+          checkRange(this.article.alt, MSG)) {
+
+        const URL   = this.constants.API_URL + "/articles/" + this.article.id;
+        const data  = new FormData();
+        let image   = document.getElementById("image").files[0] ?? this.article.image;
+
+        data.append("name", this.article.name);
+        data.append("text", this.article.text);
+        data.append("image", image);
+        data.append("alt", this.article.alt);
+        data.append("likes", JSON.stringify(this.article.likes));
+        data.append("cat", this.article.cat);
+
+        console.log(typeof data, data);
+        console.log(typeof image, image);
+
+        putData(URL, data)
+          .then(() => {
+            alert(this.article.name + this.constants.ALERT_UPDATED);
+            this.$router.push("/admin");
+          })
+          .catch(err => { setError(err) });
+      }
     }
   }
 }
