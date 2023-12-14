a89cc543c18ccf65877cbeefc7dbf29da241e902
Philippe Beck <philippe@philippebeck.net>
2023-12-14 14:36:16 +0400

    Add auth parts for signin & forgot pass to LoginView

--- src/views/base/LoginView.vue
+++ src/views/base/LoginView.vue
@@ -51,7 +51,33 @@
       </template>
 
       <template #body>
-        <AuthSet type="signin" :val="val"/>
+        <form>
+          <FieldElt type="email"
+            v-model:value="email"
+            :info="val.INFO_EMAIL">
+            <template #legend>{{ val.LEGEND_EMAIL }}</template>
+            <template #label>{{ val.LABEL_EMAIL }}</template>
+          </FieldElt>
+
+          <FieldElt type="password"
+            v-model:value="pass"
+            :info="val.INFO_PASSWORD">
+            <template #legend>{{ val.LEGEND_PASSWORD }}</template>
+            <template #label>{{ val.LABEL_PASSWORD }}</template>
+          </FieldElt>
+
+          <vue-recaptcha :sitekey="val.RECAPTCHA_KEY"
+            @verify="onVerify">
+            <BtnElt type="button"
+              class="btn-green"
+              :content="val.CONTENT_ENTER"
+              :title="val.TITLE_SIGNIN">
+              <template #btn>
+                <i class="fa-solid fa-door-open fa-lg"></i>
+              </template>
+            </BtnElt>
+          </vue-recaptcha>
+        </form>
 
         <BtnElt type="button"
           @click="setType('SignUp')"
@@ -85,7 +111,27 @@
       </template>
 
       <template #body>
-        <AuthSet type="forgot" :val="val"/>
+        <form>
+          <FieldElt type="email"
+            v-model:value="email"
+            :info="val.INFO_EMAIL"
+            required>
+            <template #legend>{{ val.LEGEND_EMAIL }}</template>
+            <template #label>{{ val.LABEL_EMAIL }}</template>
+          </FieldElt>
+
+          <vue-recaptcha :sitekey="val.RECAPTCHA_KEY"
+            @verify="onVerify">
+            <BtnElt type="button"
+              class="btn-orange"
+              :content="val.CONTENT_SEND"
+              :title="val.TITLE_FORGOT">
+              <template #btn>
+                <i class="fa-regular fa-paper-plane fa-lg"></i>
+              </template>
+            </BtnElt>
+          </vue-recaptcha>
+        </form>
 
         <BtnElt type="button"
           @click="setType('SignUp')"
@@ -112,12 +158,15 @@
 </template>
 
 <script>
+import { checkRegex, postData, setError } from "servidio"
+
 import BtnElt from "@/assets/elements/BtnElt"
 import CardElt from "@/assets/elements/CardElt"
+import FieldElt from "@/assets/elements/FieldElt"
 
-import AuthSet from "@/assets/setters/AuthSet"
 import UserSet from "@/assets/setters/UserSet"
 
+import { VueRecaptcha } from "vue-recaptcha";
 import { setMeta } from "servidio"
 
 export default {
@@ -125,13 +174,16 @@ export default {
   components: {
     BtnElt,
     CardElt,
-    AuthSet,
-    UserSet
+    FieldElt,
+    UserSet,
+    VueRecaptcha
   },
 
   props: ["val"],
   data() {
     return {
+      email: "",
+      pass: "",
       type: "SignIn"
     }
   },
@@ -156,6 +208,86 @@ export default {
     setType(type) {
       this.type = type;
     },
+
+    /**
+     * ? ON VERIFY
+     * Checks the validity of the given response 
+     * and performs signin if successful
+     *
+     * @param {any} response - The response to verify.
+     */
+    onVerify(response) {
+      const { CHECK_EMAIL, REGEX_EMAIL, CHECK_PASS, REGEX_PASS, API_URL } = this.val;
+
+      if (this.type === "signin" &&
+          checkRegex(this.email, CHECK_EMAIL, REGEX_EMAIL) && 
+          checkRegex(this.pass, CHECK_PASS, REGEX_PASS)
+          ||
+          this.type === "forgot" &&
+          checkRegex(this.email, CHECK_EMAIL, REGEX_EMAIL)) {
+
+        const URL = `${API_URL}/auth/recaptcha`;
+
+        postData(URL, { response })
+          .then(result => {
+            if (result.success) {
+              this.signIn();
+
+            } else {
+              alert("Failed captcha verification");
+            }
+          })
+          .catch(err => { 
+            setError(err);
+            this.$router.go();
+          });
+      }
+    },
+
+    /**
+     * ? SIGN IN
+     * Signs in the user.
+     */
+    signIn() {
+      const URL   = `${this.val.API_URL}/auth`;
+      const data  = new FormData();
+
+      data.append("email", this.email);
+      data.append("pass", this.pass);
+
+      postData(URL, data)
+        .then((res) => {
+          localStorage.setItem("userToken", JSON.stringify(res.token));
+          localStorage.setItem("userId", JSON.stringify(res.userId));
+
+          this.$router.go();
+        })
+        .catch(err => { setError(err) });
+    },
+
+    /**
+     * ? FORGOT PASS
+     * Executes the forgot password functionality.
+     */
+    forgotPass() {
+      const { CONFIRM_FORGOT, API_URL, FORGOT_SUBJECT, FORGOT_TEXT, ALERT_SENDED } = this.val;
+
+      if (confirm(CONFIRM_FORGOT) === true) {
+        const URL   = `${API_URL}/auth/pass`;
+        const data  = new FormData();
+
+        data.append("email", this.email);
+        data.append("subject", FORGOT_SUBJECT);
+        data.append("html", FORGOT_TEXT);
+
+        postData(URL, data)
+          .then(() => {
+            alert(FORGOT_SUBJECT + ALERT_SENDED);
+            this.$router.push("/login");
+          })
+          .catch(err => { setError(err) });
+      }
+    }
   }
 }
 </script>
