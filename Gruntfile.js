cb18f1378a127d2b394fd17e84b81aee98fb36a6
Philippe Beck <philippe@philippebeck.net>
2023-12-14 14:11:04 +0400

    Remove useless components to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,23 +12,14 @@ module.exports = function(grunt) {
       slider: { src: ["node_modules/vue-elt/components/elements/SliderElt.vue"], dest: "src/assets/elements/SliderElt.vue" },
       table: { src: ["node_modules/vue-elt/components/elements/TableElt.vue"], dest: "src/assets/elements/TableElt.vue" },
 
-      articleManager: { src: ["node_modules/vue-elt/components/managers/ArticleManager.vue"], dest: "src/assets/managers/ArticleManager.vue" },
-      galleryManager: { src: ["node_modules/vue-elt/components/managers/GalleryManager.vue"], dest: "src/assets/managers/GalleryManager.vue" },
-      imageManager: { src: ["node_modules/vue-elt/components/managers/ImageManager.vue"], dest: "src/assets/managers/ImageManager.vue" },
-      linkManager: { src: ["node_modules/vue-elt/components/managers/LinkManager.vue"], dest: "src/assets/managers/LinkManager.vue" },
-      orderManager: { src: ["node_modules/vue-elt/components/managers/OrderManager.vue"], dest: "src/assets/managers/OrderManager.vue" },
-      productManager: { src: ["node_modules/vue-elt/components/managers/ProductManager.vue"], dest: "src/assets/managers/ProductManager.vue" },
-      userManager: { src: ["node_modules/vue-elt/components/managers/UserManager.vue"], dest: "src/assets/managers/UserManager.vue" },
-
-      articleCreator: { src: ["node_modules/vue-elt/components/creators/ArticleCreator.vue"], dest: "src/assets/creators/ArticleCreator.vue" },
-      galleryCreator: { src: ["node_modules/vue-elt/components/creators/GalleryCreator.vue"], dest: "src/assets/creators/GalleryCreator.vue" },
-      imageCreator: { src: ["node_modules/vue-elt/components/creators/ImageCreator.vue"], dest: "src/assets/creators/ImageCreator.vue" },
-      linkCreator: { src: ["node_modules/vue-elt/components/creators/LinkCreator.vue"], dest: "src/assets/creators/LinkCreator.vue" },
-      productCreator: { src: ["node_modules/vue-elt/components/creators/ProductCreator.vue"], dest: "src/assets/creators/ProductCreator.vue" },
-
-      forgotPass: { src: ["node_modules/vue-elt/components/auth/ForgotPass.vue"], dest: "src/assets/auth/ForgotPass.vue" },
-      signIn: { src: ["node_modules/vue-elt/components/auth/SignIn.vue"], dest: "src/assets/auth/SignIn.vue" },
-      signUp: { src: ["node_modules/vue-elt/components/auth/SignUp.vue"], dest: "src/assets/auth/SignUp.vue" },
+      articleSet: { src: ["node_modules/vue-elt/components/setters/ArticleSet.vue"], dest: "src/assets/setters/ArticleSet.vue" },
+      authSet: { src: ["node_modules/vue-elt/components/setters/AuthSet.vue"], dest: "src/assets/setters/AuthSet.vue" },
+      gallerySet: { src: ["node_modules/vue-elt/components/setters/GallerySet.vue"], dest: "src/assets/setters/GallerySet.vue" },
+      imageSet: { src: ["node_modules/vue-elt/components/setters/ImageSet.vue"], dest: "src/assets/setters/ImageSet.vue" },
+      linkSet: { src: ["node_modules/vue-elt/components/setters/LinkSet.vue"], dest: "src/assets/setters/LinkSet.vue" },
+      orderSet: { src: ["node_modules/vue-elt/components/setters/OrderSet.vue"], dest: "src/assets/setters/OrderSet.vue" },
+      productSet: { src: ["node_modules/vue-elt/components/setters/ProductSet.vue"], dest: "src/assets/setters/ProductSet.vue" },
+      userSet: { src: ["node_modules/vue-elt/components/setters/UserSet.vue"], dest: "src/assets/setters/UserSet.vue" },
     }
   });
 
