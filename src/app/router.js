cb22fff5dd46b97b1dbab1a122a4a7708a537069
Philippe Beck <philippe@philippebeck.net>
2024-03-02 01:55:36 +0400

    Move all config files to app folder

--- src/app/router.js
+++ src/app/router.js
@@ -1,98 +1,96 @@
 import { createRouter, createWebHistory } from "vue-router"
 
 const routes = [
-  // ! MAIN
   {
     path: "/",
     name: "home",
-    component: () => import("@/views/main/HomeView"),
+    component: () => import("@/views/HomeView"),
     alias: ["/home"]
   },
   {
     path: "/contact",
     name: "contact",
-    component: () => import("@/views/main/ContactView"),
+    component: () => import("@/views/ContactView"),
     alias: ["/message"]
   },
   {
     path: "/legal",
     name: "legal",
-    component: () => import("@/views/main/LegalView"),
+    component: () => import("@/views/LegalView"),
     alias: ["/notice"]
   },
   {
     path: "/login",
     name: "login",
-    component: () => import("@/views/main/LoginView"),
+    component: () => import("@/views/LoginView"),
     alias: ["/signin"]
   },
   {
     path: "/profile",
     name: "profile",
-    component: () => import("@/views/main/ProfileView"),
+    component: () => import("@/views/ProfileView"),
     alias: ["/account"],
     meta: { requiresAuth: true }
   },
-  // ! DATA
   {
     path: "/article/:id",
     name: "article",
-    component: () => import("@/views/data/ArticleView"),
+    component: () => import("@/views/ArticleView"),
     alias: ["/blog/:id"]
   },
   {
     path: "/basket",
     name: "basket",
-    component: () => import("@/views/data/BasketView"),
+    component: () => import("@/views/BasketView"),
     alias: ["/cart"]
   },
   {
     path: "/blog",
     name: "blog",
-    component: () => import("@/views/data/BlogView"),
+    component: () => import("@/views/BlogView"),
     alias: ["/articles"]
   },
   {
     path: "/galleries",
     name: "gallery",
-    component: () => import("@/views/data/GalleryView"),
+    component: () => import("@/views/GalleryView"),
     alias: ["/images"]
   },
   {
     path: "/gallery/:id",
     name: "image",
-    component: () => import("@/views/data/ImageView"),
+    component: () => import("@/views/ImageView"),
     alias: ["/images/:id"]
   },
   {
     path: "/links",
     name: "link",
-    component: () => import("@/views/data/LinkView"),
+    component: () => import("@/views/LinkView"),
     alias: ["/link"]
   },
   {
     path: "/portfolio",
     name: "portfolio",
-    component: () => import("@/views/data/PortfolioView"),
+    component: () => import("@/views/PortfolioView"),
     alias: ["/projects"]
   },
   {
     path: "/product/:id",
     name: "product",
-    component: () => import("@/views/data/ProductView"),
+    component: () => import("@/views/ProductView"),
     alias: ["/shop/:id"]
   },
   {
     path: "/shop",
     name: "shop",
-    component: () => import("@/views/data/ShopView"),
+    component: () => import("@/views/ShopView"),
     alias: ["/products"]
   },
   // Error
   {
     path: "/:pathMatch(.*)*",
     name: "error",
-    component: () => import("@/views/main/ErrorView")
+    component: () => import("@/views/ErrorView")
   }
 ]
 
