ee980db9767927dffb42a92b7e93d7146a81c4cb
Philippe Beck <philippe@philippebeck.net>
2024-03-06 01:44:50 +0400

    Update & move App with tests

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <NavElt :items="val.NAV_MAIN">
     <template #brand>
-      <img :src="val.LOGO_SRC" :alt="val.LOGO_ALT" :width="val.LOGO_WIDTH">
+      <img :src="val.LOGO_SRC" :alt="val.LOGO_ALT" :width="val.LOGO_WIDTH" />
     </template>
 
     <template #profile>
       <a v-if="checkSession('user')" href="/profile" :title="val.PROFILE_OF + avatar.name">
-        <img :src="`/img/thumbnails/users/${avatar.image}`"
+        <img
+          :src="`/img/thumbnails/users/${avatar.image}`"
           :alt="avatar.name"
           :height="40"
           :width="40"
-          class="bord bord-circle">
+          class="bord bord-circle"
+        />
       </a>
 
       <a v-else href="/login" :title="val.TITLE_LOGIN">
@@ -20,14 +22,10 @@
   </NavElt>
 
   <router-view v-slot="{ Component }" :val="val" :avatar="avatar">
-    <component :is="Component" :key="$route.path"/>
+    <component :is="Component" :key="$route.path" />
   </router-view>
 
-  <FootElt
-    :title1="val.FOOT_TITLE1" 
-    :title2="val.FOOT_TITLE2" 
-    :title3="val.FOOT_TITLE3">
-
+  <FootElt :title1="val.FOOT_TITLE1" :title2="val.FOOT_TITLE2" :title3="val.FOOT_TITLE3">
     <template #hide>
       <i class="fa-solid fa-eye fa-fw" :title="val.TITLE_TOGGLE"></i>
     </template>
@@ -55,10 +53,12 @@
     <template #foot3>
       <ListElt :dynamic="true" :items="val.FOOT3">
         <template #items="slotProps">
-          <a :href="slotProps.item.href" 
+          <a
+            :href="slotProps.item.href"
             :title="slotProps.item.title"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             {{ slotProps.item.content }}
           </a>
         </template>
@@ -68,11 +68,13 @@
     <template #foot>
       <ListElt :dynamic="true" :items="val.FOOT">
         <template #items="slotProps">
-          <a :href="slotProps.item.href" 
+          <a
+            :href="slotProps.item.href"
             :title="slotProps.item.title"
             :aria-label="slotProps.item.title"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             <i :class="`fa-brands fa-${slotProps.item.fa} fa-lg fa-fw ${slotProps.item.color}`"></i>
           </a>
         </template>
@@ -81,15 +83,15 @@
   </FootElt>
 </template>
 
-<script>
-import val from "./app/values"
+<script lang="ts">
+import val from './assets/values'
 
-import FootElt from "./components/FootElt"
-import ListElt from "./components/ListElt"
-import NavElt from "./components/NavElt"
+import FootElt from './components/FootElt.vue'
+import ListElt from './components/ListElt.vue'
+import NavElt from './components/NavElt.vue'
 
-import { checkRole, setGlobalMeta } from "./app/services.js"
-import { mapActions, mapMutations, mapState } from "vuex"
+import { checkRole, setGlobalMeta } from './assets/services'
+import { mapActions, mapMutations, mapState } from 'vuex'
 
 export default {
   name: 'App',
@@ -106,23 +108,23 @@ export default {
    * * Gets the Id & the token from the store
    * * Checks if the token is valid to read the avatar
    */
-  created() {
-    this.val = val;
-    setGlobalMeta(val.LANG, val.ICON);
+  created(): void {
+    this.val = val
+    setGlobalMeta(val.LANG, val.ICON)
 
-    this.$store.commit("setId");
-    this.$store.commit("setToken");
+    this.$store.commit('setId')
+    this.$store.commit('setToken')
 
-    if (this.token) this.$store.dispatch("readAvatar", this.id);
+    if (this.token) this.$store.dispatch('readAvatar', this.id)
   },
 
   computed: {
-    ...mapState(["avatar", "id", "token"]),
+    ...mapState(['avatar', 'id', 'token'])
   },
 
   methods: {
-    ...mapMutations(["setId", "setToken"]),
-    ...mapActions(["readAvatar"]),
+    ...mapMutations(['setId', 'setToken']),
+    ...mapActions(['readAvatar']),
 
     /**
      * ? CHECK SESSION
@@ -130,7 +132,9 @@ export default {
      * @param {string} role - the role to check
      * @return {boolean} the result of the check
      */
-    checkSession(role) { return checkRole(this.avatar.role, role) }
+    checkSession(role: string): boolean {
+      return checkRole(this.avatar.role, role)
+    }
   }
-};
+}
 </script>
